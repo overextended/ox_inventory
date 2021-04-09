e55209f571c55b96fae5563cc67a59c29493066f
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-09 15:38:31 +1000

    Stashes moved here

---
+++ shared/stashes.lua
@@ -0,0 +1,6 @@
+Config.PoliceEvidence = vector3(474.2242, -990.7516, 26.2638) -- /evidence # while near this point
+
+Config.Stashes = {
+	--{ coords = vector3(474.2242, -990.7516, 26.2638), slots = 71, name = 'Police Evidence', job = 'police' }, using command instead
+	{ coords = vector3(301.4374, -599.2748, 43.2821), slots = 71, name = 'Hospital Cloakroom', job = 'ambulance'  }
+}
