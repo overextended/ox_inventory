a81dc9226cdc97ee954fd4d4be25c05b49a25b27
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-09 14:55:19 +1000

    Added vehicle config

---
+++ client/vehicles.lua
@@ -0,0 +1,77 @@
+-- 0		vehicle has no storage
+-- 1		vehicle storage in the hood
+Config.VehicleStorage = {
+	[`jester`]=1, 
+	[`adder`]=1, 
+	[`osiris`]=0, 
+	[`pfister811`]=0, 
+	[`penetrator`]=0, 
+	[`autarch`]=0, 
+	[`bullet`]=0, 
+	[`cheetah`]=0, 
+	[`cyclone`]=0, 
+	[`voltic`]=0, 
+	[`reaper`]=1, 
+	[`entityxf`]=0, 
+	[`t20`]=0, 
+	[`taipan`]=0, 
+	[`tezeract`]=0, 
+	[`torero`]=1, 
+	[`turismor`]=0, 
+	[`fmj`]=0, 
+	[`infernus `]=0, 
+	[`italigtb`]=1, 
+	[`italigtb2`]=1, 
+	[`nero2`]=0, 
+	[`vacca`]=1, 
+	[`vagner`]=0, 
+	[`visione`]=0, 
+	[`prototipo`]=0, 
+	[`zentorno`]=0
+}
+
+Config.GloveboxSlots = {
+	[0] = 11, -- compact
+	[1] = 11, -- sedan
+	[2] = 11, -- suv
+	[3] = 11, -- coupe
+	[4] = 11, -- muscle
+	[5] = 11, -- sports classic
+	[6] = 11, -- sports
+	[7] = 11, -- super
+	[8] = 5, -- motorcycle
+	[9] = 11, -- offroad
+	[10] = 11, -- industrial
+	[11] = 11, -- utility
+	[12] = 11, -- van
+	[14] = 31, -- boat
+	[15] = 31, -- helicopter
+	[16] = 51, -- plane
+	[17] = 11, -- service
+	[18] = 11, -- emergency
+	[19] = 11, -- military
+	[20] = 11, -- commercial (trucks)
+}
+
+Config.TrunkSlots = {
+	[0] = 21, -- compact
+	[1] = 41, -- sedan
+	[2] = 51, -- suv
+	[3] = 31, -- coupe
+	[4] = 41, -- muscle
+	[5] = 31, -- sports classic
+	[6] = 31, -- sports
+	[7] = 21, -- super
+	[8] = 5, -- motorcycle
+	[9] = 51, -- offroad
+	[10] = 51, -- industrial
+	[11] = 41, -- utility
+	[12] = 61, -- van
+	--[14] = 21, -- boat		no trunk
+	--[15] = 21, -- helicopter	no trunk
+	--[16] = 21, -- plane		no trunk
+	[17] = 41, -- service
+	[18] = 41, -- emergency
+	[19] = 41, -- military
+	[20] = 61, -- commercial
+}
