3938799803a6c76daa602f25c79aefcf7501d0fe
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-12 00:49:32 +1000

    Config descriptions | Added Config.AutoReload

--- config.lua
+++ config.lua
@@ -1,10 +1,24 @@
 Config = {}
 
+-- Should be a multiple of 5 + 1
 Config.PlayerSlots = 51
+
+-- 30kg is a lot to carry
 Config.PlayerWeight = 30000
+
+-- Blur the screen while in an inventory
 Config.EnableBlur = true
+
+-- Requires esx_licenses
 Config.WeaponsLicense = true
 Config.WeaponsLicensePrice = 5000
+
+-- Requires setup; I'll release something in the future
 Config.Logs = false
+
+-- Default keymapping for the inventory; players can assign their own
 Config.InventoryKey = 'F2'
 Config.VehicleInventoryKey = 'K'
+
+-- Reload empty weapons automatically
+Config.AutoReload = false
