6551de072e1cdade5fc5ce76f3422632c0f2b914
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-17 09:41:04 +1000

    No longer need to send invid

--- server/functions.lua
+++ server/functions.lua
@@ -122,7 +122,8 @@ ValidateItem = function(type, xPlayer, fromSlot, toSlot, fromItem, toItem)
 	else return true end
 end 
 
-ItemNotify = function(xPlayer, item, count, slot, type, invid)
+ItemNotify = function(xPlayer, item, count, slot, type)
+	local player = Inventories[xPlayer.source]
 	if Items[item.name] then TriggerClientEvent('linden_inventory:itemNotify', xPlayer.source, item, count, slot, type) end
 end
 
@@ -147,7 +148,7 @@ CreateNewDrop = function(xPlayer, data)
 	if data.type == 'freeslot' then
 		if ValidateItem(data.type, xPlayer, Inventories[invid2].inventory[data.emptyslot], Drops[invid].inventory[data.toSlot], data.item, data.item) == true then
 			local count = Inventories[invid2].inventory[data.emptyslot].count
-			ItemNotify(xPlayer, data.item, count, data.emptyslot, 'Removed', invid)
+			ItemNotify(xPlayer, data.item, count, data.emptyslot, 'Removed')
 			Inventories[invid2].inventory[data.emptyslot] = nil
 			Drops[invid].inventory[data.toSlot] = {name = data.item.name, label = data.item.label, weight = data.item.weight, slot = data.toSlot, count = data.item.count, description = data.item.description, metadata = data.item.metadata, stackable = data.item.stackable, closeonuse = Items[data.item.name].closeonuse}
 			if Config.Logs then
@@ -157,7 +158,7 @@ CreateNewDrop = function(xPlayer, data)
 		end
 	elseif data.type == 'split' then
 		if ValidateItem(data.type, xPlayer, Inventories[invid2].inventory[data.fromSlot], Drops[invid].inventory[data.toSlot], data.oldslotItem, data.newslotItem) == true then
-			ItemNotify(xPlayer, data.newslotItem, data.newslotItem.count, data.fromSlot, 'Removed', invid)
+			ItemNotify(xPlayer, data.newslotItem, data.newslotItem.count, data.fromSlot, 'Removed')
 			Inventories[invid2].inventory[data.fromSlot] = {name = data.oldslotItem.name, label = data.oldslotItem.label, weight = data.oldslotItem.weight, slot = data.fromSlot, count = data.oldslotItem.count, description = data.oldslotItem.description, metadata = data.oldslotItem.metadata, stackable = data.oldslotItem.stackable, closeonuse = Items[data.oldslotItem.name].closeonuse}
 			Drops[invid].inventory[data.toSlot] = {name = data.newslotItem.name, label = data.newslotItem.label, weight = data.newslotItem.weight, slot = data.toSlot, count = data.newslotItem.count, description = data.newslotItem.description, metadata = data.newslotItem.metadata, stackable = data.newslotItem.stackable, closeonuse = Items[data.newslotItem.name].closeonuse}
 			if Config.Logs then
