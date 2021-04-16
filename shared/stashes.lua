cad198f4d3af57fcf957eec359460240b6fcfaf4
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-16 10:06:19 +1000

    Add a note about stashes

--- shared/stashes.lua
+++ shared/stashes.lua
@@ -1,6 +1,8 @@
 Config.PoliceEvidence = vector3(474.2242, -990.7516, 26.2638) -- /evidence # while near this point
 
+-- Do not define job for anyone to have access to the stash
+-- Property stashes need to be handled with your property resource using the export (look at Snippets)
 Config.Stashes = {
 	--{ coords = vector3(474.2242, -990.7516, 26.2638), slots = 70, name = 'Police Evidence', job = 'police' }, using command instead
-	{ coords = vector3(301.4374, -599.2748, 43.2821), slots = 70, name = 'Hospital Cloakroom', job = 'ambulance'  }
+	{ coords = vector3(301.4374, -599.2748, 43.2821), slots = 70, name = 'Hospital Cloakroom', job = 'ambulance' },
 }
