61aaafe3814111af9cb4e88f40e093bdb25dcc0b
sahtiyan <81315836+sahtiyan@users.noreply.github.com>
2021-04-17 07:34:13 +0300

    Update vehicles.lua

--- client/vehicles.lua
+++ client/vehicles.lua
@@ -1,38 +1,5 @@
--- 0	vehicle has no storage
--- 1	vehicle storage in the hood
-Config.VehicleStorage = {
-	[`jester`]=1, 
-	[`adder`]=1, 
-	[`osiris`]=0, 
-	[`pfister811`]=0, 
-	[`penetrator`]=0, 
-	[`autarch`]=0, 
-	[`bullet`]=0, 
-	[`cheetah`]=0, 
-	[`cyclone`]=0, 
-	[`voltic`]=0, 
-	[`reaper`]=1, 
-	[`entityxf`]=0, 
-	[`t20`]=0, 
-	[`taipan`]=0, 
-	[`tezeract`]=0, 
-	[`torero`]=1, 
-	[`turismor`]=0, 
-	[`fmj`]=0, 
-	[`infernus `]=0, 
-	[`italigtb`]=1, 
-	[`italigtb2`]=1, 
-	[`nero2`]=0, 
-	[`vacca`]=1, 
-	[`vagner`]=0, 
-	[`visione`]=0, 
-	[`prototipo`]=0, 
-	[`zentorno`]=0
-}
-
-
 -- slots, maxWeight; default weight is 8000 per slot
-Config.Gloveboxes = {	
+Config.Gloveboxes = {
 	[0] = {11, 88000}, -- compact
 	[1] = {11, 88000}, -- sedan
 	[2] = {11, 88000}, -- suv
@@ -52,28 +19,60 @@ Config.Gloveboxes = {
 	[17] = {11, 88000}, -- service
 	[18] = {11, 88000}, -- emergency
 	[19] = {11, 88000}, -- military
-	[20] = {11, 88000} -- commercial (trucks)
+	[20] = {11, 88000}, -- commercial (trucks)
+
+	[`jester`] = {5, 488000}
 }
 
 Config.Trunks = {
-	[0] = {21, 168000}, -- compact
-	[1] = {41, 328000}, -- sedan
-	[2] = {51, 408000}, -- suv
-	[3] = {31, 248000}, -- coupe
-	[4] = {41, 328000}, -- muscle
-	[5] = {31, 248000}, -- sports classic
-	[6] = {31, 248000}, -- sports
-	[7] = {21, 168000}, -- super
-	[8] = {5,  40000},-- motorcycle
-	[9] = {51, 408000}, -- offroad
-	[10] = {51, 408000}, -- industrial
-	[11] = {41, 328000}, -- utility
-	[12] = {61, 488000}, -- van
-	--[14] -- boat
-	--[15] -- helicopter
-	--[16] -- plane
-	[17] = {41, 328000}, -- service
-	[18] = {41, 328000}, -- emergency
-	[19] = {41, 328000}, -- military
-	[20] = {61, 488000} -- commercial
+	--ON/OFF, SLOT, Weight
+	--0:off,1:front,2:back
+	[0] = {2,21, 168000}, -- compact
+	[1] = {2,41, 328000}, -- sedan
+	[2] = {2,51, 408000}, -- suv
+	[3] = {2,31, 248000}, -- coupe
+	[4] = {2,41, 328000}, -- muscle
+	[5] = {2,31, 248000}, -- sports classic
+	[6] = {1,31, 248000}, -- sports
+	[7] = {1,21, 168000}, -- super
+	[8] = {0},-- motorcycle
+	[9] = {2,51, 408000}, -- offroad
+	[10] = {2,51, 408000}, -- industrial
+	[11] = {2,41, 328000}, -- utility
+	[12] = {2,61, 488000}, -- van
+	[14] = {0}, -- boat
+	[15] = {0}, -- helicopter
+	[16] = {0}, -- plane
+	[17] = {2,41, 328000}, -- service
+	[18] = {2,41, 328000}, -- emergency
+	[19] = {1,41, 328000}, -- military
+	[20] = {2,61, 488000}, -- commercial
+
+	[`jester`]={1,5,30000}, 
+	[`adder`]={1,5,30000},
+	[`torero`]={1,5,30000},
+	[`reaper`]={1,5,30000},
+	[`italigtb`]={1,5,30000}, 
+	[`italigtb2`]={1,5,30000},
+	[`vacca`]={1,5,30000}, 
+	[`osiris`]={0}, 
+	[`pfister811`]={0}, 
+	[`penetrator`]={0}, 
+	[`autarch`]={0}, 
+	[`bullet`]={0}, 
+	[`cheetah`]={0}, 
+	[`cyclone`]={0}, 
+	[`voltic`]={0}, 
+	[`entityxf`]={0}, 
+	[`t20`]={0}, 
+	[`taipan`]={0}, 
+	[`tezeract`]={0}, 
+	[`turismor`]={0}, 
+	[`fmj`]={0}, 
+	[`infernus `]={0}, 
+	[`nero2`]={0}, 
+	[`vagner`]={0}, 
+	[`visione`]={0}, 
+	[`prototipo`]={0}, 
+	[`zentorno`]={0}
 }
