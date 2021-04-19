c445f6014e16e1399f33345ba5479cae47e786c5
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-20 05:15:31 +1000

    Ensure an item is usable before sending an event to the server | Prevent infinite gun reloading without removing an item

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,6 @@
 local Blips = {}
 local Drops = {}
+local Usables = {}
 local currentDrop
 local currentWeapon
 local weaponTimer = 0
@@ -40,6 +41,7 @@ StartInventory = function()
 		playerID = GetPlayerServerId(PlayerId())
 		playerName = data.name
 		Drops = data.drops
+		Usables = data.usables
 		inventoryLabel = playerName..' ['..playerID..'] '--[[..ESX.PlayerData.job.grade_label]]
 		PlayerLoaded = true
 		ClearWeapons()
@@ -383,20 +385,22 @@ end)
 
 RegisterNetEvent('linden_inventory:addAmmo')
 AddEventHandler('linden_inventory:addAmmo', function(ammo)
-	if currentWeapon then
+	if currentWeapon and not isBusy then
 		if currentWeapon.ammoType == ammo.name then
 			local maxAmmo = GetWeaponClipSize(currentWeapon.hash)
 			local curAmmo = GetAmmoInPedWeapon(playerPed, currentWeapon.hash)
 			if curAmmo > maxAmmo then SetPedAmmo(playerPed, currentWeapon.hash, maxAmmo) elseif curAmmo == maxAmmo then return
 			else
+				isBusy = true
 				local newAmmo = 0
 				if curAmmo < maxAmmo then missingAmmo = maxAmmo - curAmmo end
-				if missingAmmo > ammo.count then newAmmo = ammo.count + curAmmo removeAmmo = ammo.count - curAmmo
-				else newAmmo = maxAmmo removeAmmo = missingAmmo end
+				if missingAmmo > ammo.count then newAmmo = ammo.count + curAmmo
+				else newAmmo = maxAmmo end
 				if newAmmo < 0 then newAmmo = 0 end
 				SetPedAmmo(playerPed, currentWeapon.hash, newAmmo)
 				MakePedReload(playerPed)
-				TriggerServerEvent('linden_inventory:addweaponAmmo', currentWeapon, removeAmmo, newAmmo)
+				TriggerServerEvent('linden_inventory:addweaponAmmo', currentWeapon, curAmmo, newAmmo)
+				isBusy = false
 			end
 		else
 			error("You can't load the "..currentWeapon.label.." with "..ammo.label.." ammo")
@@ -451,13 +455,10 @@ TriggerLoops = function()
 				DisableControlAction(0, 257, true)
 			elseif not invOpen and not wait and CanOpenInventory() then
 				for i=1, #Keys, 1 do
-					if IsDisabledControlJustReleased(0, Keys[i]) then
-						Citizen.CreateThread(function()
-							wait = true
+					if not isBusy and IsDisabledControlJustReleased(0, Keys[i]) and ESX.PlayerData.inventory[i] then
+						if Usables[ESX.PlayerData.inventory[i].name] then
 							TriggerServerEvent('linden_inventory:useSlotItem', i)
-							Citizen.Wait(100)
-							wait = false
-						end)
+						end
 					end
 				end
 			end
@@ -793,7 +794,7 @@ RegisterNUICallback('notification', function(data)
 end)
 
 RegisterNUICallback('useItem', function(data, cb)
-	if data.inv == 'Playerinv' then TriggerServerEvent('linden_inventory:useItem', data.item) end
+	if data.inv == 'Playerinv' and Usables[data.item.name] then TriggerServerEvent('linden_inventory:useItem', data.item) end
 end)
 
 RegisterNUICallback('giveItem', function(data, cb)
