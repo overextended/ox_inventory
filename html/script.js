16e02bf7e78df99e23a38e44e9f85f8c02dd2c6f
joshphotoshop <joshphotoshop@gmail.com>
2021-05-08 17:45:04 +0800

    Limit draggable item to inventory bounds

--- html/script.js
+++ html/script.js
@@ -338,7 +338,7 @@ function DragAndDrop() {
 		revertDuration: 0,
 		revert: "invalid",
 		cancel: ".itemdragclose",
-		containment: "body",
+		containment: "parent",
 		start: function(event, ui) {
 			IsDragging = true;
 			$(this).find("img").css("filter", "brightness(50%)");
