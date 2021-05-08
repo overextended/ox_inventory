a32425637df698e89e4b82918e9576cae76e6479
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-09 04:41:32 +1000

    Forgot to replace inform function | Armour event

--- client/items.lua
+++ client/items.lua
@@ -1,17 +1,17 @@
 AddEventHandler('linden_inventory:burger', function()
-	inform('You ate a delicious burger')
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You ate a delicious burger', length = 2500})
 end)
 
 AddEventHandler('linden_inventory:water', function()
-	inform('You drank some refreshing water')
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You drank some refreshing water', length = 2500})
 end)
 
 AddEventHandler('linden_inventory:cola', function()
-	inform('You drank some delicious eCola')
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You drank some delicious eCola', length = 2500})
 end)
 
 AddEventHandler('linden_inventory:mustard', function()
-	inform('You.. drank mustard')
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You.. drank mustard', length = 2500})
 end)
 
 AddEventHandler('linden_inventory:bandage', function()
@@ -19,4 +19,9 @@ AddEventHandler('linden_inventory:bandage', function()
 	local health = GetEntityHealth(playerPed)
 	local newHealth = math.min(maxHealth, math.floor(health + maxHealth / 16))
 	SetEntityHealth(playerPed, newHealth)
+end)
+
+AddEventHandler('linden_inventory:armour', function()
+	SetPlayerMaxArmour(PlayerId(), 100)
+	SetPedArmour(playerPed, 100)
 end)
