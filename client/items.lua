485684071e38fff05240b253c0a6505248170a4c
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-23 12:28:32 +1000

    improvement(client/items): Send callbacks immediately then wait for the use time

--- client/items.lua
+++ client/items.lua
@@ -1,42 +1,48 @@
 AddEventHandler('linden_inventory:burger', function(item, cb)
-	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You ate a delicious burger', length = 2500})
 	cb(true)
+	Citizen.Wait(wait)
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You ate a delicious burger', length = 2500})
 end)
 
 AddEventHandler('linden_inventory:water', function(item, cb)
+	cb(true)
+	Citizen.Wait(wait)
 	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You drank some refreshing water', length = 2500})
-	cb(true)
 end)
 
-AddEventHandler('linden_inventory:cola', function(item, cb)
+AddEventHandler('linden_inventory:cola', function(item, wait, cb)
+	cb(true)
+	Citizen.Wait(wait)
 	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You drank some delicious eCola', length = 2500})
-	cb(true)
 end)
 
-AddEventHandler('linden_inventory:mustard', function(item, cb)
+AddEventHandler('linden_inventory:mustard', function(item, wait, cb)
+	cb(true)
+	Citizen.Wait(wait)
 	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You.. drank mustard', length = 2500})
-	cb(true)
 end)
 
-AddEventHandler('linden_inventory:bandage', function(item, cb)
+AddEventHandler('linden_inventory:bandage', function(item, wait, cb)
 	local maxHealth = 200
 	local health = GetEntityHealth(ESX.PlayerData.ped)
 	if health < maxHealth then
+		cb(true)
+		Citizen.Wait(wait)
 		local newHealth = math.min(maxHealth, math.floor(health + maxHealth / 16))
 		SetEntityHealth(ESX.PlayerData.ped, newHealth)
 		TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You feel better already', length = 2500})
-		cb(true)
 	else cb(false) end
 end)
 
-AddEventHandler('linden_inventory:armour', function(item, cb)
+AddEventHandler('linden_inventory:armour', function(item, wait, cb)
+	cb(true)
+	Citizen.Wait(wait)
 	SetPlayerMaxArmour(playerID, 100)
 	SetPedArmour(ESX.PlayerData.ped, 100)
-	cb(true)
 end)
 
 --[[		On hold, giving me an aneurysm to get the weight system working properly
-AddEventHandler('linden_inventory:paperbag', function(item, cb)
+AddEventHandler('linden_inventory:paperbag', function(item, wait, cb)
 	if item.name == 'paperbag' then
 		OpenStash({ item = {slot=item.slot, name=item.name}, slots = 3, maxWeight = 1000, name = item.metadata.type, label = 'Paper bag ('..item.metadata.type..')' })
 	end
