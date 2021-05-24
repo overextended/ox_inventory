32c5d53f9f70c83b320b19c2f263e095a0647b57
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-25 07:12:26 +1000

    chore(sql/update): Add alter table query

--- update.sql
+++ update.sql
@@ -1,3 +1,9 @@
+-- Update for 1.5.3
+ALTER TABLE `linden_inventory`
+	CHANGE COLUMN `owner` `owner` VARCHAR(60) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci' FIRST,
+	CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci' AFTER `owner`;
+
+
 -- Update for 1.5.2
 ALTER TABLE `linden_inventory`
 	DROP COLUMN `id`;
@@ -5,5 +11,5 @@ ALTER TABLE `linden_inventory`
 
 -- Update for 1.5.0
 ALTER TABLE `linden_inventory`
-    ADD COLUMN `lastupdated` TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP();
+	ADD COLUMN `lastupdated` TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP();
     
