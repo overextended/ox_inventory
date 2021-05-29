c76c7061410009d5838e3287de4d3fbce61df363
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-30 02:21:03 +1000

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,7 +1,10 @@
--- Update for 1.5.3
+-- Run these queries separately if the query for 1.5.3 does not work
 UPDATE linden_inventory SET OWNER = '' WHERE OWNER IS NULL
+
 UPDATE linden_inventory SET NAME = '' WHERE NAME IS NULL
 
+
+-- Update for 1.5.3
 ALTER TABLE `linden_inventory`
 	CHANGE COLUMN `owner` `owner` VARCHAR(60) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci' FIRST,
 	CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci' AFTER `owner`;
