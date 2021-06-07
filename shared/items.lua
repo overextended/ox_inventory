37ddbe0cfdd03e4f7f07bdf3c436d5963a0025bf
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-07 12:12:45 +1000

    fix(shared/items): Typo

--- shared/items.lua
+++ shared/items.lua
@@ -8,7 +8,7 @@ Items = {
 		client = {
 			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
 			prop = { model = 'prop_rolled_sock_02', pos = { x = -0.13999999999998, y = -0.13999999999998, y = -0.080000000000012}, rot = { x = -50.0, y = -50.0, y = 0.0} },
-			disable = { move = true, car = true, combat = true }
+			disable = { move = true, car = true, combat = true },
 			usetime = 2500,
 			event = true,
 		}
