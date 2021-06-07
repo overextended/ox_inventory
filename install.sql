401eae45bc2daa64f8c0c2f8d291957ddfa126ad
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-07 18:22:35 +1000

    revert(sql): Keep the table structure for items

--- install.sql
+++ install.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS `linden_inventory` (
 	
 REPLACE INTO `licenses` (`type`, `label`) VALUES
 	('weapon', "Weapons license");
+
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `label` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `weight` int(11) NOT NULL DEFAULT 1,
+  `can_remove` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;	
+
+ALTER TABLE `items`
+	ADD IF NOT EXISTS `stackable` TINYINT(1) NULL DEFAULT '1', 
+	ADD IF NOT EXISTS `closeonuse` TINYINT(1) NULL DEFAULT '1', 
+	ADD IF NOT EXISTS `description` VARCHAR(50) NULL DEFAULT NULL,
+	ADD PRIMARY KEY IF NOT EXISTS (`name`);
