6e6aaf0176e90838af8a66fded9f772204f1f9f0
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-13 19:07:55 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 author 'Linden'
 description 'https://github.com/thelindat/linden_inventory'
 versioncheck 'https://raw.githubusercontent.com/thelindat/linden_inventory/main/fxmanifest.lua'
-version '1.7.0'
+version '1.7.2'
 
 dependencies {
 	'es_extended',
