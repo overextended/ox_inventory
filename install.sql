a71e6e1647d7f89bcb9c6e5f8052c3deb52b7f5f
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-14 22:58:42 +1000

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `linden_inventory` (
-    `owner` VARCHAR(60) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-    `name` VARCHAR(100) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci', 
-    `data` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `owner` VARCHAR(60) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci',
+    `name` VARCHAR(100) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci', 
+    `data` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
     `lastupdated` TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
     UNIQUE INDEX (`name`, `owner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
