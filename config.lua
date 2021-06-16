90074adeefba8e63f5c51ab26ccb05513bfde490
Leah <na>
2021-06-16 22:52:57 +0100

    bt-target Support for Shops

--- config.lua
+++ config.lua
@@ -35,3 +35,6 @@ Config.AutoReload = false
 
 -- Randomise the price of items in each shop at resource start
 Config.RandomPrices = false
+
+-- If you use bt-target and want to make use of bt-target on the shops enable this.
+Config.bt_target = true
