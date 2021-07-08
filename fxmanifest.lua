ab0517b14c7300815f810ac64f9330ff41ca3116
Linden <heliowaltonau@gmail.com>
2021-07-09 01:51:58 +1000

    1.8.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 author 'Linden'
 description 'https://github.com/thelindat/linden_inventory'
 versioncheck 'https://raw.githubusercontent.com/thelindat/linden_inventory/main/fxmanifest.lua'
-version '1.7.7'
+version '1.8.0'
 
 dependencies {
 	'es_extended',
