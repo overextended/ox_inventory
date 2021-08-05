a5086e50ed36259cdb7d3e9ec7fda299b065934f
dunak-debug <erikbalog@hotmail.com>
2021-08-05 17:11:56 +0200

    refactor(nui): progress bar

--- html/src/components/utils/ProgressBar.tsx
+++ html/src/components/utils/ProgressBar.tsx
@@ -1,45 +1,43 @@
-import { useState } from "react"
-import useNuiEvent from "../../hooks/useNuiEvent"
-import { debugData } from "../../utils/debugData"
+import { useState } from "react";
+import useNuiEvent from "../../hooks/useNuiEvent";
+import { debugData } from "../../utils/debugData";
 
 debugData([
-    {
-      action: 'startProgress',
-      data: {
-        text: "Using Water",
-        duration: 5000,
-      }
-    }
-])
+  {
+    action: "startProgress",
+    data: {
+      text: "Using Water",
+      duration: 5000,
+    },
+  },
+]);
 
 const ProgressBar: React.FC = () => {
-    const [duration, setDuration] = useState<number>(0)
-    const [isVisible, setVisible] = useState<boolean>(false)
-    const [text, setText] = useState<string>('')
+  const [duration, setDuration] = useState(0);
+  const [isVisible, setVisible] = useState(false);
+  const [text, setText] = useState("");
 
-    useNuiEvent('startProgress', (data) => {
+  useNuiEvent("startProgress", (data) => {
+    setText(data.text);
+    setDuration(data.duration);
+    setVisible(true);
+  });
 
-        setText(data.text)
-        
-        setDuration(data.duration)
-
-        setVisible(true)
-        
-        setTimeout(() => {
-            setVisible(false)
-        }, data.duration)
-    })
-
-    // Shouldn't need with style in progress when using keyframe anim
-    return (
-        <div className="progbar-container" style={{visibility: isVisible ? 'visible' : 'hidden'}}>
-            <div className="progress" style={{/*width: isVisible ? '100%' : '0%',*/ animation: isVisible ? `progbar ${duration}ms forwards` : ''}}> 
-                <div className="progbar-text">
-                    {text}
-                </div>
-            </div>
+  return (
+    <>
+      {isVisible && (
+        <div className="progressBar">
+          <div
+            className="progressBar-value"
+            style={{ animationDuration: `${duration}ms` }}
+            onAnimationEnd={() => setVisible(false)}
+          >
+            <span>{text}</span>
+          </div>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
