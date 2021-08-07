b9af523863964a2ec437da85407d26a4cd64a6df
Linden <65407488+thelindat@users.noreply.github.com>
2021-08-07 18:49:53 +1000

    refactor(modules): Minor optimisation to code
    Defining the resource file before loading it uses extra memory
    No need to pass an environment to the chunk either

--- modules/main.lua
+++ modules/main.lua
@@ -37,8 +37,7 @@ module = function(file, shared)
 	if not Modules[file] then
 		local path = shared and file or ox.server and 'server/'..file or 'client/'..file
 		path = 'modules/'..path..'.lua'
-		local resourceFile = LoadResourceFile(Config.Resource, path)
-		local func, err = load(resourceFile, path, 't', _G)
+		local func, err = load(LoadResourceFile(Config.Resource, path), path, 't')
 		assert(func, err == nil or '\n^1'..err..'^7')
 		Modules[file] = func()
 	end
