777f448fa61e235b81954892f2cef9b55d022208
dunak-debug <erikbalog@hotmail.com>
2021-08-09 01:42:44 +0200

    fix(nui): fix refreshSlots null values

--- html/src/reducers/refreshSlots.ts
+++ html/src/reducers/refreshSlots.ts
@@ -12,13 +12,17 @@ export const refreshSlots: CaseReducer<
   >
 > = (state, action) => {
   state.isBusy = true;
-  action.payload.forEach((data) => {
-    const inventory =
-      data.inventory === undefined
-        ? state.playerInventory
-        : state.rightInventory;
+  action.payload
+    .filter((value) => value !== null)
+    .forEach((data, index) => {
+      const inventory =
+        data.inventory === undefined
+          ? state.playerInventory
+          : state.rightInventory;
 
-    inventory.items[data.item.slot - 1] = data.item;
-  });
+      inventory.items[data.item.slot - 1] = data.item || {
+        slot: index + 1,
+      };
+    });
   state.isBusy = false;
 };
