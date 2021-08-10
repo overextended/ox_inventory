ab3db88facedf1cb00a9816d49abe8fd156db889
dunak-debug <19434539+dunak-debug@users.noreply.github.com>
2021-08-11 01:52:52 +0200

    refactor(nui): maybe finally swapping works?

--- html/src/reducers/setupInventory.ts
+++ html/src/reducers/setupInventory.ts
@@ -10,24 +10,22 @@ export const setupInventoryReducer: CaseReducer<
 > = (state, action) => {
   state.playerInventory = {
     ...action.payload.playerInventory,
-    items: Array.from(Array(action.payload.playerInventory.slots), (_, k) => ({
-      slot: k + 1,
-    })),
+    items: Array.from(
+      Array(action.payload.playerInventory.slots),
+      (_, index) =>
+        Object.values(action.payload.playerInventory.items).find(
+          (item) => item?.slot === index + 1
+        ) || { slot: index + 1 }
+    ),
   };
-  for (let item of Object.values(action.payload.playerInventory.items).filter(
-    (value) => value
-  )) {
-    state.playerInventory.items[item.slot - 1] = item;
-  }
   state.rightInventory = {
     ...action.payload.rightInventory,
-    items: Array.from(Array(action.payload.rightInventory.slots), (_, k) => ({
-      slot: k + 1,
-    })),
+    items: Array.from(
+      Array(action.payload.rightInventory.slots),
+      (_, index) =>
+        Object.values(action.payload.rightInventory.items).find(
+          (item) => item?.slot === index + 1
+        ) || { slot: index + 1 }
+    ),
   };
-  for (let item of Object.values(action.payload.rightInventory.items).filter(
-    (value) => value
-  )) {
-    state.rightInventory.items[item.slot - 1] = item;
-  }
 };
