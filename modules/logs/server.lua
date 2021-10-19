b5ae6c3be38fbdb71d9541af7705f3aea73017c2
Linden <65407488+thelindat@users.noreply.github.com>
2021-10-20 06:28:06 +1100

    refactor(modules/logs): Separate file execution logic

--- modules/logs/server.lua
+++ modules/logs/server.lua
@@ -3,27 +3,23 @@ local os = os
 local day, month, year = string.strsplit('-', os.date('%d-%B-%Y'))
 local path = GetResourcePath(ox.name):gsub('//', '/')
 path = ('%s/modules/logs'):format(path)
-local l = '/'
-
-local function unix()
-	local system = os.getenv('OS')
-	if not system or system:match('Windows') then
-		unix = false
-		l = '\\'
-	else unix = true end
-end
-unix()
 
 local _format = string.format
-local function dir(str, ...)
-	str = _format(str, ...):gsub('/', l)
-	return str
+
+local function init()
+	local file = _format('%s-%s-%s.json', year, month, day)
+	local system = os.getenv('OS')
+	if system and system:match('Windows') then
+		path = path:gsub('/', '\\')
+		os.execute(_format('mkdir %s\\%s\\%s', path, year, month))
+		return io.open(_format('%s\\%s\\%s\\%s', path, year, month, file), 'a+')
+	else
+		os.execute(_format('mkdir -p %s/%s/%s', path, year, month))
+		return io.open(_format('%s/%s/%s/%s', path, year, month, file), 'a+')
+	end
 end
 
-os.execute(dir('%s %s%s%s%s%s', unix and 'mkdir -p' or 'mkdir', path, l, year, l, month))
-
-local fml = dir('%s-%s-%s.json', year, month, day)
-local file = io.open(dir('%s%s%s%s%s%s%s', path, l, year, l, month, l, fml), 'a+')
+local file = init()
 
 if file then
 	local time = '%H:%M:%S'
