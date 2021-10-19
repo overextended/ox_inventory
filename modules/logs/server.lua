f9512d9ce3b7de7515ce3ad24979c10e9348a9fc
Linden <65407488+thelindat@users.noreply.github.com>
2021-10-20 06:17:32 +1100

    fix(modules/logs): Support nested directory creation on unix
    - Added some basic sanity checking to prevent inventory from failing to load

--- modules/logs/server.lua
+++ modules/logs/server.lua
@@ -3,29 +3,40 @@ local os = os
 local day, month, year = string.strsplit('-', os.date('%d-%B-%Y'))
 local path = GetResourcePath(ox.name):gsub('//', '/')
 path = ('%s/modules/logs'):format(path)
+local l = '/'
 
-local l = os.getenv('OS')
-if not l or not l:match('Windows') then
-	l = '/' else l = '\\'
+local function unix()
+	local system = os.getenv('OS')
+	if not system or system:match('Windows') then
+		unix = false
+		l = '\\'
+	else unix = true end
 end
+unix()
 
 local _format = string.format
-local function format(str, ...)
+local function dir(str, ...)
 	str = _format(str, ...):gsub('/', l)
 	return str
 end
 
-os.execute(format('mkdir %s%s%s%s%s', path, l, year, l, month))
+os.execute(dir('%s %s%s%s%s%s', unix and 'mkdir -p' or 'mkdir', path, l, year, l, month))
 
-local fml = ('%s-%s-%s.json'):format(year, month, day)
-local file = io.open(format('%s%s%s%s%s%s%s', path, l, year, l, month, l, fml), 'a+')
+local fml = dir('%s-%s-%s.json', year, month, day)
+local file = io.open(dir('%s%s%s%s%s%s%s', path, l, year, l, month, l, fml), 'a+')
 
-local time = '%H:%M:%S'
-local string = _format([[{ "date": "%s/%s/%s", "time": "%s", "content": "%s" },
-]], day, month:sub(0, 3), year, '%s', '%s')
+if file then
+	local time = '%H:%M:%S'
+	local string = _format([[{ "date": "%s/%s/%s", "time": "%s", "content": "%s" },
+	]], day, month:sub(0, 3), year, '%s', '%s')
 
-local write = function(message)
-	file:write(_format(string, os.date(time), message))
+	local write = function(message)
+		file:write(_format(string, os.date(time), message))
+	end
+
+	return write
+else
+	ox.warning('Unable to initilise logging module')
+	local function write() return end
+	return write
 end
-
-return write
