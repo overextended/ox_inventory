d632938ff02541be14819de16bd19fba5afe8eec
Linden <65407488+thelindat@users.noreply.github.com>
2021-10-25 09:03:08 +1100

    refactor(data/stashes): Use jobs table for access

--- data/stashes.lua
+++ data/stashes.lua
@@ -1,4 +1,4 @@
 return {
-	{ coords = vec3(452.3, -991.4, 30.7), name = 'policelocker', label = 'Personal locker', owner = true, slots = 70, weight = 70000, job = 'police' },
-	{ coords = vec3(301.3, -600.23, 43.28), name = 'emslocker', label = 'Personal Locker', owner = true, slots = 70, weight = 70000, job = 'ambulance' }
+	{ coords = vec3(452.3, -991.4, 30.7), name = 'policelocker', label = 'Personal locker', owner = true, slots = 70, weight = 70000, jobs = {['police'] = 0} },
+	{ coords = vec3(301.3, -600.23, 43.28), name = 'emslocker', label = 'Personal Locker', owner = true, slots = 70, weight = 70000, jobs = {['ambulance'] = 0} }
 }
