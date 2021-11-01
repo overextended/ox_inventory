5c5cbacb43fc57d557a1ca8a7b3960fe6e6bcf25
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-01 17:23:01 +1100

    refactor(client): properly utilise metatables for shop/stashes modules
    Indexing the modules will simply check through the data as normal (rather than doing Shops.Stores).
    
    With the __call metamethod we can call the functions for creating the markers, blips, etc.

--- modules/shops/client.lua
+++ modules/shops/client.lua
@@ -1,7 +1,6 @@
+local M = data('shops')
 
 local Blips = {}
-local Stores = data('shops')
-
 local CreateLocationBlip = function(blipId, name, blip, location)
 	Blips[blipId] = AddBlipForCoord(location.x, location.y)
 	SetBlipSprite(Blips[blipId], blip.id)
@@ -14,55 +13,54 @@ local CreateLocationBlip = function(blipId, name, blip, location)
 	EndTextCommandSetBlipName(Blips[blipId])
 end
 
-local CreateShopLocations = function()
-	if next(Blips) then
-		for i=1, #Blips do RemoveBlip(Blips[i]) end
-		table.wipe(Blips)
-	end
-	local blipId = 0
-	for type, shop in pairs(Stores) do
-		if shop.jobs == nil or (shop.jobs[ESX.PlayerData.job.name] and ESX.PlayerData.job.grade >= shop.jobs[ESX.PlayerData.job.name]) then
-			blipId += 1
-			if Config.Target then
-				local OpenShop = function(data)
-					TriggerEvent('ox_inventory:openInventory', 'shop', data)
-				end
-				for id=1, #shop.targets do
-					local target = shop.targets[id]
-					local shopid = type..'-'..id
-					exports.qtarget:RemoveZone(shopid)
-					if shop.blip then CreateLocationBlip(blipId, shop.name, shop.blip, target.loc) end
-					exports.qtarget:AddBoxZone(shopid, target.loc, target.length or 0.5, target.width or 0.5, {
-						name=shopid,
-						heading=target.heading or 0.0,
-						debugPoly=false,
-						minZ=target.minZ,
-						maxZ=target.maxZ
-					}, {
-						options = {
-							{
-								icon = "fas fa-shopping-basket",
-								label = ox.locale('open_shop', shop.name),
-								job = shop.job,
-								action = function()
-									OpenShop({id=id, type=type})
-								end
+setmetatable(M, {
+	__call = function()
+		if next(Blips) then
+			for i=1, #Blips do RemoveBlip(Blips[i]) end
+			table.wipe(Blips)
+		end
+		local blipId = 0
+		for type, shop in pairs(M) do
+			if shop.jobs == nil or (shop.jobs[ESX.PlayerData.job.name] and ESX.PlayerData.job.grade >= shop.jobs[ESX.PlayerData.job.name]) then
+				blipId += 1
+				if Config.Target then
+					local OpenShop = function(data)
+						TriggerEvent('ox_inventory:openInventory', 'shop', data)
+					end
+					for id=1, #shop.targets do
+						local target = shop.targets[id]
+						local shopid = type..'-'..id
+						exports.qtarget:RemoveZone(shopid)
+						if shop.blip then CreateLocationBlip(blipId, shop.name, shop.blip, target.loc) end
+						exports.qtarget:AddBoxZone(shopid, target.loc, target.length or 0.5, target.width or 0.5, {
+							name=shopid,
+							heading=target.heading or 0.0,
+							debugPoly=false,
+							minZ=target.minZ,
+							maxZ=target.maxZ
+						}, {
+							options = {
+								{
+									icon = "fas fa-shopping-basket",
+									label = ox.locale('open_shop', shop.name),
+									job = shop.job,
+									action = function()
+										OpenShop({id=id, type=type})
+									end
+								},
 							},
-						},
-						distance = target.distance or 3.0
-					})
-				end
-			elseif shop.blip then
-				for i=1, #shop.locations do
-					blipId += 1
-					CreateLocationBlip(blipId, shop.name, shop.blip, shop.locations[i])
+							distance = target.distance or 3.0
+						})
+					end
+				elseif shop.blip then
+					for i=1, #shop.locations do
+						blipId += 1
+						CreateLocationBlip(blipId, shop.name, shop.blip, shop.locations[i])
+					end
 				end
 			end
 		end
 	end
-end
+})
 
-return {
-	CreateShopLocations = CreateShopLocations,
-	Stores = Stores
-}
+return M
--- modules/stashes/client.lua
+++ modules/stashes/client.lua
@@ -1,38 +1,34 @@
-local Stashes <const> = data('stashes')
-local M = {}
+local M = data('stashes')
 
 setmetatable(M, {
     __call = function()
-        return Stashes
-    end
-})
-
-M.CreateStashes = function()
-    for _, stash in pairs(Stashes) do
-        if Config.Target then
-            local OpenStash = function(data) TriggerEvent('ox_inventory:openInventory', 'stash', data) end
-            exports.qtarget:RemoveZone(stash.name)
-            exports.qtarget:AddBoxZone(stash.name, stash.target.loc, stash.target.length or 0.5, stash.target.width or 0.5, {
-                name=stash.name,
-                heading=stash.target.heading or 0.0,
-                debugPoly=false,
-                minZ=stash.target.minZ,
-                maxZ=stash.target.maxZ
-            }, {
-                options = {
-                    {
-                        icon = "fas fa-warehouse",
-                        label = "Open Stash",
-                        job = stash.jobs,
-                        action = function()
-                            OpenStash(stash)
-                        end
+        for _, stash in pairs(M) do
+            if Config.Target then
+                local OpenStash = function(data) TriggerEvent('ox_inventory:openInventory', 'stash', data) end
+                exports.qtarget:RemoveZone(stash.name)
+                exports.qtarget:AddBoxZone(stash.name, stash.target.loc, stash.target.length or 0.5, stash.target.width or 0.5,
+                {
+                    name=stash.name,
+                    heading=stash.target.heading or 0.0,
+                    debugPoly=false,
+                    minZ=stash.target.minZ,
+                    maxZ=stash.target.maxZ
+                }, {
+                    options = {
+                        {
+                            icon = "fas fa-warehouse",
+                            label = "Open Stash",
+                            job = stash.jobs,
+                            action = function()
+                                OpenStash(stash)
+                            end
+                        },
                     },
-                },
-                distance = stash.target.distance or 3.0
-            })
+                    distance = stash.target.distance or 3.0
+                })
+            end
         end
     end
-end
+})
 
 return M
