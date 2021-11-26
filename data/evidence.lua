e0d84664423a3e9ab2cc6008f8df1a22d7c4ccf1
PyschoShock <94499608+PsychoShock@users.noreply.github.com>
2021-11-25 23:00:08 -0500

    fix(typo): From uniuque to unique

--- data/evidence.lua
+++ data/evidence.lua
@@ -2,7 +2,7 @@ return {
 	{
 		coords = vec3(458.97, -982.79, 30.68),
 		target = { -- qtarget support
-			name = 'mrpd_evidence', -- name of zone must be uniuqe
+			name = 'mrpd_evidence', -- name of zone must be unique
 			loc = vec3(459.07, -984.07, 30.69),
 			length = 1.4,
 			width = 3.2,
