d35db38d466e7d1818997ae85d22b5856e5e5173
Linden <65407488+thelindat@users.noreply.github.com>
2021-12-01 16:56:09 +0000

    chore: update dependencies

--- web/craco.config.js
+++ web/craco.config.js
@@ -1,24 +1,25 @@
-const path = require("path");
+const path = require('path');
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       // Because CEF has issues with loading source maps properly atm,
       // lets use the best we can get in line with `eval-source-map`
       if (webpackConfig.mode === 'development' && process.env.IN_GAME_DEV) {
-        webpackConfig.devtool = 'eval-source-map'
-        webpackConfig.output.path = path.join(__dirname, 'build')
+        webpackConfig.devtool = 'eval-source-map';
+        webpackConfig.output.path = path.join(__dirname, 'build');
       }
 
-      return webpackConfig
-    }
+      return webpackConfig;
+    },
   },
 
   devServer: (devServerConfig) => {
     if (process.env.IN_GAME_DEV) {
-     // Used for in-game dev mode
-     devServerConfig.writeToDisk = true
+      // Used for in-game dev mode
+      devServerConfig.writeToDisk = true;
     }
 
-    return devServerConfig
-  }
-}
+    return devServerConfig;
+  },
+};
