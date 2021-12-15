ad48063c474574335b46abbc801da64e0fc10d95
Linden <65407488+thelindat@users.noreply.github.com>
2021-12-15 03:29:10 +0000

    chore(sql): remove collation

--- setup/install.sql
+++ setup/install.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `ox_inventory` (
-	`owner` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-	`data` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+	`owner` varchar(60) DEFAULT NULL,
+	`name` varchar(100) NOT NULL,
+	`data` longtext DEFAULT NULL,
 	`lastupdated` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
 	UNIQUE KEY `owner` (`owner`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 -- Setup new columns for vehicle stashes, and actually index owner
 ALTER TABLE `owned_vehicles`
--- setup/upgrade.sql
+++ setup/upgrade.sql
@@ -1,9 +1,9 @@
 -- Update existing linden_inventory table to new name and indexes
 ALTER TABLE `linden_inventory`
 	RENAME TO `ox_inventory`,
-	CHANGE COLUMN `owner` `owner` VARCHAR(60) NULL COLLATE 'utf8mb4_unicode_ci' FIRST,
-	CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `owner`,
-	CHANGE COLUMN `data` `data` LONGTEXT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `name`,
+	CHANGE COLUMN `owner` `owner` VARCHAR(60) NULL FIRST,
+	CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL AFTER `owner`,
+	CHANGE COLUMN `data` `data` LONGTEXT NULL AFTER `name`,
 	DROP INDEX `name`,
 	ADD UNIQUE INDEX `owner` (`owner`, `name`);
 
