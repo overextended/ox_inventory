d3699bfa59c0ef6bb541791e4bf1e92891bf4d22
Linden <65407488+thelindat@users.noreply.github.com>
2022-01-01 03:50:46 +1100

    feat(init): prevent files from loading if an error is thrown
    If any of the version checks or the build check fail, a single error should display instead of one from each file.

--- modules/init.lua
+++ modules/init.lua
@@ -3,23 +3,8 @@ function ox.info(...) ox.print('^2[info]^7', ...) end
 function ox.warning(...) ox.print('^3[warning]^7', ...) end
 function ox.error(...) error('\n^1[error] '.. ... ..'^7', 2) end
 
--- Not really necessary since it's set as a dependency
-if not SetInterval or not import then
-	ox.error('Ox Inventory requires the pe-lualib resource, refer to the documentation.')
-else
-	local version = GetResourceMetadata('pe-lualib', 'version', 0)
-	if version < '1.2.0' then
-		ox.error('A more recent version of pe-lualib is required.')
-	end
-end
-
--- Disable qtarget compatibility if it isn't running
-if ox.qtarget and GetResourceState('qtarget') ~= 'started' then
-	ox.qtarget = false
-	ox.info('qtarget is not running; disabled compatibility mode')
-end
-
 function data(name)
+	if ox.ready == nil then return {} end
 	local func, err = load(LoadResourceFile(ox.resource, ('data/%s.lua'):format(name)))
 	if err then error('^1'..err..'^0', 0) end
 	return func()
@@ -31,3 +16,31 @@ function ox.locale(string, ...)
 	if Locales[string] then return Locales[string]:format(...) end
 	return string
 end
+
+do
+	if not SetInterval or not import then
+		ox.error('Ox Inventory requires the pe-lualib resource, refer to the documentation.')
+	else
+		local version = GetResourceMetadata('pe-lualib', 'version', 0) or 0
+		if version < '1.2.1' then
+			ox.error('A more recent version of pe-lualib is required.')
+		end
+	end
+
+	local version = GetResourceMetadata('oxmysql', 'version', 0) or 0
+	if version < '1.9.0' then
+		ox.error('A more recent version of oxmysql is required.')
+	end
+
+	if not LoadResourceFile(ox.resource, 'web/build/index.html') then
+		ox.error('Unable to locate ox_inventory/web/build, refer to the documentation or download a release build.')
+	end
+end
+
+-- Disable qtarget compatibility if it isn't running
+if ox.qtarget and GetResourceState('qtarget') ~= 'started' then
+	ox.qtarget = false
+	ox.info('qtarget is not running; disabled compatibility mode')
+end
+
+ox.ready = false
