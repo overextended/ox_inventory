fa03c92f6d863911596e186522aaafa1de1e812e
Linden <65407488+thelindat@users.noreply.github.com>
2022-01-23 13:01:03 +1100

    update version

--- version.lua
+++ version.lua
@@ -5,18 +5,18 @@ if server.versioncheck then
         local url = GetResourceMetadata(resource, 'repository', 0)
         local version = GetResourceMetadata(resource, 'version', 0)
 
-        PerformHttpRequest(('%s/master/fxmanifest.lua'):format(url:gsub('github.com', 'raw.githubusercontent.com')), function(error, response)
-            if error == 200 and response then
+        PerformHttpRequest(('%s/master/fxmanifest.lua'):format(url:gsub('github.com', 'raw.githubusercontent.com')), function(status, response)
+            if response and status == 200 then
                 local latest = response:match('%d%.%d+%.%d+')
                 if version < latest then
                     print(('^3An update is available for ox_inventory - please download the latest release (current version: %s)'):format(latest, version))
 print([[^3	- https://github.com/overextended/ox_inventory/releases
-	- rebalanced weapon durability loss
-	- support multiple police jobs
-	- player inventories no longer "lock" after using a container
-	- remove invalid durability when loading an inventory
-	- all weapons are removed from players when disarming
-	- various tweaks to code flow and structure
+    - new convars, with old config removed (refer to config.cfg)
+    - new logging via datadog
+	- client tracks total item count
+	- updateInventory event to track item movements
+    - integrated NPWD support with phone item
+    - bug fixes
 ^0]])
                 end
             end
