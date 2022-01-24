26c2ff1ea9ae106e459d655aa973d19c9316cda5
Linden <65407488+thelindat@users.noreply.github.com>
2022-01-24 17:54:43 +1100

    update version

--- version.lua
+++ version.lua
@@ -10,14 +10,7 @@ if server.versioncheck then
                 local latest = response:match('%d%.%d+%.%d+')
                 if version < latest then
                     print(('^3An update is available for ox_inventory - please download the latest release (current version: %s)'):format(latest, version))
-print([[^3	- https://github.com/overextended/ox_inventory/releases
-    - new convars, with old config removed (refer to config.cfg)
-    - new logging via datadog
-	- client tracks total item count
-	- updateInventory event to track item movements
-    - integrated NPWD support with phone item
-    - bug fixes
-^0]])
+print('^3	- https://github.com/overextended/ox_inventory/releases^0')
                 end
             end
         end, 'GET')
