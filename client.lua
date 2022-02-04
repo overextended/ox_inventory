762bb5bf04d8457fa4df3d12c3f2ff7366f7bc20
ANTOND <71350868+antond15@users.noreply.github.com>
2022-02-04 23:29:36 +0100

    revert: updateInventory event and invalid weapons

--- client.lua
+++ client.lua
@@ -672,6 +672,7 @@ local function updateInventory(items, weight)
 	end
 
 	client.setPlayerData('inventory', PlayerData.inventory)
+	TriggerEvent('ox_inventory:updateInventory', changes)
 end
 
 RegisterNetEvent('ox_inventory:updateSlots', function(items, weights, count, removed)
@@ -817,6 +818,7 @@ RegisterNetEvent('ox_inventory:setPlayerInventory', function(currentDrops, inven
 	plyState:set('invBusy', false, false)
 	plyState:set('invOpen', false, false)
 	PlayerData.loaded = true
+	TriggerEvent('ox_inventory:updateInventory', PlayerData.inventory)
 
 	Utils.Notify({text = shared.locale('inventory_setup'), duration = 2500})
 	local Licenses = data 'licenses'
