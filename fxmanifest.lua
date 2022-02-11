28359fac1bc66090914520b22a10ffb0e79d5a0f
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2022-02-11 12:30:31 +0100

    chore: Update manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game         'gta5'
 --[[ Resource Information ]]--
 name         'ox_inventory'
 author       'Overextended'
-version      '2.3.2'
+version      '2.3.5'
 repository   'https://github.com/overextended/ox_inventory'
 description  'Slot-based inventory with metadata'
 
