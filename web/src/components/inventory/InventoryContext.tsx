25a43a899e8c40af3d16aef024be1852f85b249c
tfkiraly <risitashun@gmail.com>
2022-03-19 10:42:11 +0100

    Fix the case when weapon not have serial

--- web/src/components/inventory/InventoryContext.tsx
+++ web/src/components/inventory/InventoryContext.tsx
@@ -65,7 +65,7 @@ const InventoryContext: React.FC<{
           <Item onClick={handleClick} data={{ action: 'drop' }}>
             {Locale.ui_drop}
           </Item>
-          {props.item.metadata?.serial && (
+          {props.item.hashname.startsWith('WEAPON_') && props.item.metadata?.serial && (
             <>
               <Separator />
               <Item
