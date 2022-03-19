b6f9497b83b3ad9f2319fafa43bef54f001ba831
Linden <65407488+thelindat@users.noreply.github.com>
2022-03-19 03:06:07 +0000

    chore(fxmanifest): version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game         'gta5'
 --[[ Resource Information ]]--
 name         'ox_inventory'
 author       'Overextended'
-version      '2.6.0'
+version      '2.6.1'
 repository   'https://github.com/overextended/ox_inventory'
 description  'Slot-based inventory with metadata'
 
