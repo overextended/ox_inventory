58a352d13f4643e8c8c74b21826ffd1f801f4b58
Linden <65407488+thelindat@users.noreply.github.com>
2022-04-11 09:00:27 +1000

    chore: update version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game         'gta5'
 --[[ Resource Information ]]--
 name         'ox_inventory'
 author       'Overextended'
-version      '2.7.3'
+version      '2.7.4'
 repository   'https://github.com/overextended/ox_inventory'
 description  'What even is an "ESX.Items" anyway?'
 
