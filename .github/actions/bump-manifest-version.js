dbbfc0bf670f535533fd494d36d1ee39e9022339
Luke <sabolukas03@gmail.com>
2022-04-13 23:21:03 +0200

    ci(releases): Automatic fxmanifest version bump

---
+++ .github/actions/bump-manifest-version.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const version = process.env.TGT_RELEASE_VERSION
+const newVersion = version.replace('v', '')
+
+const manifestFile = fs.readFileSync('fxmanifest.lua', {encoding: 'utf8'})
+
+const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version '${newVersion}'`)
+
+fs.writeFileSync('fxmanifest.lua', newFileContent) 
