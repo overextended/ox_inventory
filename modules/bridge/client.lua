1af73cc63603287c7c410a6bc880aaa33d4ba8e5
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-01 13:26:32 +1000

    refactor(bridge/client): lazy ESX handler
    Request the ESX "object" when it's first indexed.
    Also removes the requestPlayerInventory trigger.

--- modules/bridge/client.lua
+++ modules/bridge/client.lua
@@ -47,21 +47,15 @@ if shared.framework == 'ox' then
 	end)
 
 elseif shared.framework == 'esx' then
-	local ESX
-
-	SetTimeout(1500, function()
-		ESX = exports.es_extended:getSharedObject()
-
-		ESX = {
-			SetPlayerData = ESX.SetPlayerData,
-			PlayerLoaded = ESX.PlayerLoaded
-		}
-
-		if ESX.PlayerLoaded then
-			TriggerServerEvent('ox_inventory:requestPlayerInventory')
+	local ESX = table.create(0, 2)
+	setmetatable(ESX, {
+		__index = function(self, index)
+			local obj = exports.es_extended:getSharedObject()
+			ESX.SetPlayerData = obj.SetPlayerData
+			ESX.PlayerLoaded = obj.PlayerLoaded
+			return ESX[index]
 		end
-	end)
-
+	})
 
 	function client.setPlayerData(key, value)
 		PlayerData[key] = value
