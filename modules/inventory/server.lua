overextended/ox_inventory

commit ef86c59a0d5aa5702f76e5ee6d119fd2112c1b1e

refactor(server/inventory): change metadata type checks
Change metadata type checks from 'string' to 'table', assigning a new
table with type = metadata when appropriate.

modules/inventory/server.lua
@@ -484,7 +484,7 @@ function Inventory.GetItem(inv, item, metadata, returnsCount)
 
 		if inv then
 			local ostime = os.time()
-			metadata = not metadata and false or type(metadata) == 'string' and {type=metadata} or metadata
+			metadata = not metadata and false or type(metadata) == 'table' and metadata or  { type = metadata }
 
 			for _, v in pairs(inv.items) do
 				if v and v.name == item.name and (not metadata or table.contains(v.metadata, metadata)) then
@@ -614,7 +614,7 @@ function Inventory.SetMetadata(inv, slot, metadata)
 	slot = type(slot) == 'number' and (inv and inv.items[slot])
 	if inv and slot then
 		if inv then
-			slot.metadata = type(metadata) == 'table' and metadata or {type = metadata}
+			slot.metadata = type(metadata) == 'table' and metadata or { type = metadata }
 
 			if metadata.weight then
 				inv.weight -= slot.weight
@@ -716,7 +716,7 @@ function Inventory.Search(inv, search, items, metadata)
 
 			if search == 'slots' then search = 1 elseif search == 'count' then search = 2 end
 			if type(items) == 'string' then items = {items} end
-			if type(metadata) == 'string' then metadata = {type=metadata} end
+			if type(metadata) ~= 'table' then metadata = { type = metadata } end
 
 			local itemCount = #items
 			local returnData = {}
@@ -787,8 +787,8 @@ function Inventory.RemoveItem(inv, item, count, metadata, slot)
 	if item and count > 0 then
 		inv = Inventory(inv)
 
-		if metadata ~= nil then
-			metadata = type(metadata) == 'string' and {type=metadata} or metadata
+		if metadata then
+			metadata = type(metadata) == 'table' and metadata or { type = metadata }
 		end
 
 		local itemSlots, totalCount = Inventory.GetItemSlots(inv, item, metadata)
@@ -844,7 +844,7 @@ function Inventory.CanCarryItem(inv, item, count, metadata)
 	if type(item) ~= 'table' then item = Items(item) end
 	if item then
 		inv = Inventory(inv)
-		local itemSlots, totalCount, emptySlots = Inventory.GetItemSlots(inv, item, metadata == nil and {} or type(metadata) == 'string' and {type=metadata} or metadata)
+		local itemSlots, totalCount, emptySlots = Inventory.GetItemSlots(inv, item, metadata == nil and {} or type(metadata) == 'table' and metadata or { type = metadata })
 		local weight = metadata?.weight or item.weight
 
 		if next(itemSlots) or emptySlots > 0 then

commit 495511270a211e4d153c54345b0360c3de23a84c

fix(server/inventory): metadata type checks
Broken in ef86c59.
Some checks would assign metadata.type as false, where nil would
be expected, which broke metadata matching.
Naturally it was reported 2 minutes after pushing a tag.

modules/inventory/server.lua
@@ -484,7 +484,10 @@ function Inventory.GetItem(inv, item, metadata, returnsCount)
 
 		if inv then
 			local ostime = os.time()
-			metadata = not metadata and false or type(metadata) == 'table' and metadata or  { type = metadata }
+
+			if type(metadata) ~= 'table' then
+				metadata = metadata and { type = metadata or nil }
+			end
 
 			for _, v in pairs(inv.items) do
 				if v and v.name == item.name and (not metadata or table.contains(v.metadata, metadata)) then
@@ -614,7 +617,9 @@ function Inventory.SetMetadata(inv, slot, metadata)
 	slot = type(slot) == 'number' and (inv and inv.items[slot])
 	if inv and slot then
 		if inv then
-			slot.metadata = type(metadata) == 'table' and metadata or { type = metadata }
+			if type(metadata) ~= 'table' then
+				slot.metadata = metadata and { type = metadata or nil }
+			end
 
 			if metadata.weight then
 				inv.weight -= slot.weight
@@ -716,7 +721,10 @@ function Inventory.Search(inv, search, items, metadata)
 
 			if search == 'slots' then search = 1 elseif search == 'count' then search = 2 end
 			if type(items) == 'string' then items = {items} end
-			if type(metadata) ~= 'table' then metadata = { type = metadata } end
+
+			if type(metadata) ~= 'table' then
+				metadata = metadata and { type = metadata or nil }
+			end
 
 			local itemCount = #items
 			local returnData = {}
@@ -787,8 +795,8 @@ function Inventory.RemoveItem(inv, item, count, metadata, slot)
 	if item and count > 0 then
 		inv = Inventory(inv)
 
-		if metadata then
-			metadata = type(metadata) == 'table' and metadata or { type = metadata }
+		if type(metadata) ~= 'table' then
+			metadata = metadata and { type = metadata or nil }
 		end
 
 		local itemSlots, totalCount = Inventory.GetItemSlots(inv, item, metadata)