30b9c55c48af674ec222c51ab5ffa441c7c6f1fd
Luke <sabolukas03@gmail.com>
2022-09-04 18:17:53 +0200

    fix(web): don't try to display tooltip when dragging an item

--- web/src/components/inventory/InventorySlot.tsx
+++ web/src/components/inventory/InventorySlot.tsx
@@ -136,7 +136,7 @@ const InventorySlot: React.FC<SlotProps> = ({ inventory, item }) => {
 
   return (
     <Tooltip
-      title={!isSlotWithItem(item) ? '' : <SlotTooltip item={item} />}
+      title={!isSlotWithItem(item) || isOver || isDragging ? '' : <SlotTooltip item={item} />}
       sx={(theme) => ({ fontFamily: theme.typography.fontFamily })}
       disableInteractive
       followCursor
