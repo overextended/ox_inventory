c5cb719ae3335da1aebf98ed9105e8cb1243471d
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-09-09 13:07:23 +0200

    fix: removed duplicated files

--- nd/client.lua
+++
@@ -1,7 +0,0 @@
-RegisterNetEvent("ND:setCharacter", function(character)
-    local groups = {}
-    for group, info in pairs(character.groups) do
-        groups[group] = info.lvl
-    end
-    OnPlayerData("groups", groups)
-end)
