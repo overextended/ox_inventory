3f4e39d660e5ea7c35ef459380d360af9a6fe8d0
Cyril Beeckman <cyril.beeckman@gmail.com>
2022-10-23 01:47:16 +0200

    Param radio

--- data/items.lua
+++ data/items.lua
@@ -57,9 +57,18 @@ return {
 
   ['radio'] = {
     label = 'Radio',
-    weight = 1000,
-    stack = false,
-    allowArmed = true
+    weight = 100,
+    stack = true,
+    close = true,
+    client = {
+      export = 'ac_radio.openRadio',
+      remove = function(total)
+        -- Disconnets a player from the radio when all his radio items are removed.
+        if total < 1 and GetConvar('radio_noRadioDisconnect', 'true') == 'true' then
+          exports.ac_radio:leaveRadio()
+        end
+      end
+    }
   },
 
   ['armour'] = {
