d9bf0c3b27d4ea089f4b86f65d8d83800fc763f3
Linden <65407488+thelindat@users.noreply.github.com>
2022-11-11 08:15:09 +1100

    refactor(qb/convert): utilise transactions
    Revert inventory conversion in the event of an error (a common theme for qb).
    Splits up trunk and glovebox queries to reduce load a little.

--- setup/convert.lua
+++ setup/convert.lua
@@ -146,13 +146,12 @@ local function ConvertQB()
 	local count = 0
 	local parameters = {}
 
-	Print(('Converting %s user inventories to new data format'):format(total))
-
 	for i = 1, #users do
 		count += 1
 		local inventory, slot = {}, 0
-		local items = users[i].inventory and json.decode(users[i].inventory) or {}
-		local accounts = users[i].money and json.decode(users[i].accounts) or {}
+		local user = users[i]
+		local items = user.inventory and json.decode(user.inventory) or {}
+		local accounts = user.money and json.decode(user.accounts) or {}
 
 		for k, v in pairs(accounts) do
 			if type(v) == 'table' then break end
@@ -175,14 +174,17 @@ local function ConvertQB()
 			end
 		end
 
-		parameters[count] = {json.encode(inventory), users[i].citizenid}
+		parameters[count] = { 'UPDATE players SET inventory = ? WHERE citizenid = ?', { json.encode(inventory), user.citizenid } }
 	end
 
-	MySQL.prepare.await('UPDATE players SET inventory = ? WHERE citizenid = ?', parameters)
+	Print(('Converting %s user inventories to new data format'):format(count))
 
-	-- Throw an error if ox_inventory does not exist
-	-- User needs to use upgrade.sql first
-	MySQL.query.await('SELECT name FROM ox_inventory')
+	if count > 0 then
+		if not MySQL.transaction.await(parameters) then
+			return Print('An error occurred while converting player inventories')
+		end
+		Wait(100)
+	end
 
 	local plates = MySQL.query.await('SELECT plate, citizenid FROM player_vehicles')
 
@@ -234,25 +236,40 @@ local function ConvertQB()
 				end
 			end
 
-			Print(('Moving ^3%s^0 trunks and ^3%s^0 gloveboxes to player_vehicles table'):format(#trunk, #glovebox))
-			parameters = {}
+			Print(('Moving ^3%s^0 trunks and to the player_vehicles table'):format(#trunk))
+			table.wipe(parameters)
 			count = 0
 
 			for owner, v in pairs(vehicles) do
 				for plate, v2 in pairs(v) do
 					count += 1
-					parameters[count] = {
-						v2.trunk,
-						v2.glovebox,
-						plate,
-						owner
-					}
+					parameters[count] = { 'UPDATE player_vehicles SET trunk = ? WHERE plate = ? AND citizenid = ?', { v2.trunk, plate, owner } }
 				end
 			end
 
+			if count > 0 then
+				if not MySQL.transaction.await(parameters) then
+					return Print('An error occurred while converting trunk inventories')
+				end
 
-			if #parameters > 0 then
-				MySQL.prepare.await('UPDATE player_vehicles SET trunk = ?, glovebox = ? WHERE plate = ? AND citizenid = ?', parameters)
+				Wait(100)
+			end
+
+			Print(('Moving ^3%s^0 gloveboxes and to the player_vehicles table'):format(#glovebox))
+			table.wipe(parameters)
+			count = 0
+
+			for owner, v in pairs(vehicles) do
+				for plate, v2 in pairs(v) do
+					count += 1
+					parameters[count] = { 'UPDATE player_vehicles SET glovebox = ? WHERE plate = ? AND citizenid = ?', { v2.glovebox, plate, owner } }
+				end
+			end
+
+			if count > 0 then
+				if not MySQL.transaction.await(parameters) then
+					return Print('An error occurred while converting glovebox inventories')
+				end
 			end
 		end
 	end
