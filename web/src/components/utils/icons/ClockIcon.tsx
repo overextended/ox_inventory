921fdec574dcd0d65330d90fc128b0d6bb5f73be
Luke <sabolukas03@gmail.com>
2022-11-11 14:41:51 +0100

    fix(web/toolip): fix invalid DOM props for clock icon

--- web/src/components/utils/icons/ClockIcon.tsx
+++ web/src/components/utils/icons/ClockIcon.tsx
@@ -5,11 +5,11 @@ export const ClockIcon: React.FC = () => {
       width="14"
       height="14"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="#ffffff"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <circle cx="12" cy="12" r="9" />
