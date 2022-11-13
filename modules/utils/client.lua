5134585bf3c396945c13c11da7b0c283a63c4455
Cyril Beeckman <cyril.beeckman@gmail.com>
2022-11-13 19:19:42 +0100

    fix

--- modules/utils/client.lua
+++ modules/utils/client.lua
@@ -3,98 +3,102 @@ if not lib then return end
 local Utils = {}
 
 function Utils.PlayAnim(wait, dict, name, blendIn, blendOut, duration, flag, rate, lockX, lockY, lockZ)
-	lib.requestAnimDict(dict)
-	CreateThread(function()
-		TaskPlayAnim(cache.ped, dict, name, blendIn, blendOut, duration, flag, rate, lockX, lockY, lockZ)
-		Wait(wait)
-		if wait > 0 then ClearPedSecondaryTask(cache.ped) end
-	end)
+  lib.requestAnimDict(dict)
+  CreateThread(function()
+    TaskPlayAnim(cache.ped, dict, name, blendIn, blendOut, duration, flag, rate, lockX, lockY, lockZ)
+    Wait(wait)
+    if wait > 0 then ClearPedSecondaryTask(cache.ped) end
+  end)
 end
 
-function Utils.PlayAnimAdvanced(wait, dict, name, posX, posY, posZ, rotX, rotY, rotZ, animEnter, animExit, duration, flag, time)
-	lib.requestAnimDict(dict)
-	CreateThread(function()
-		TaskPlayAnimAdvanced(cache.ped, dict, name, posX, posY, posZ, rotX, rotY, rotZ, animEnter, animExit, duration, flag, time, 0, 0)
-		Wait(wait)
-		if wait > 0 then ClearPedSecondaryTask(cache.ped) end
-	end)
+function Utils.PlayAnimAdvanced(wait, dict, name, posX, posY, posZ, rotX, rotY, rotZ, animEnter, animExit, duration, flag
+                                , time)
+  lib.requestAnimDict(dict)
+  CreateThread(function()
+    TaskPlayAnimAdvanced(cache.ped, dict, name, posX, posY, posZ, rotX, rotY, rotZ, animEnter, animExit, duration, flag,
+      time, 0, 0)
+    Wait(wait)
+    if wait > 0 then ClearPedSecondaryTask(cache.ped) end
+  end)
 end
 
 function Utils.Raycast(flag)
-	local playerCoords = GetEntityCoords(cache.ped)
-	local plyOffset = GetOffsetFromEntityInWorldCoords(cache.ped, 0.0, 2.2, -0.25)
-	local rayHandle = StartShapeTestCapsule(playerCoords.x, playerCoords.y, playerCoords.z + 0.5, plyOffset.x, plyOffset.y, plyOffset.z, 2.2, flag or 30, cache.ped, 4)
-	while true do
-		Wait(0)
-		local result, _, _, _, entityHit = GetShapeTestResult(rayHandle)
-		if result ~= 1 then
-			local entityType
-			if entityHit then entityType = GetEntityType(entityHit) end
-			if entityHit and entityType ~= 0 then
-				return entityHit, entityType
-			end
-			return false
-		end
-	end
+  local playerCoords = GetEntityCoords(cache.ped)
+  local plyOffset = GetOffsetFromEntityInWorldCoords(cache.ped, 0.0, 2.2, -0.25)
+  local rayHandle = StartShapeTestCapsule(playerCoords.x, playerCoords.y, playerCoords.z + 0.5, plyOffset.x, plyOffset.y
+    , plyOffset.z, 2.2, flag or 30, cache.ped, 4)
+  while true do
+    Wait(0)
+    local result, _, _, _, entityHit = GetShapeTestResult(rayHandle)
+    if result ~= 1 then
+      local entityType
+      if entityHit then entityType = GetEntityType(entityHit) end
+      if entityHit and entityType ~= 0 then
+        return entityHit, entityType
+      end
+      return false
+    end
+  end
 end
 
 function Utils.GetClosestPlayer()
-	local players = GetActivePlayers()
-	local playerCoords = GetEntityCoords(cache.ped)
-	local targetDistance, targetId, targetPed
+  local players = GetActivePlayers()
+  local playerCoords = GetEntityCoords(cache.ped)
+  local targetDistance, targetId, targetPed
 
-	for i = 1, #players do
-		local player = players[i]
+  for i = 1, #players do
+    local player = players[i]
 
-		if player ~= cache.playerId then
-			local ped = GetPlayerPed(player)
-			local distance = #(playerCoords - GetEntityCoords(ped))
+    if player ~= cache.playerId then
+      local ped = GetPlayerPed(player)
+      local distance = #(playerCoords - GetEntityCoords(ped))
 
-			if distance < (targetDistance or 2) then
-				targetDistance = distance
-				targetId = player
-				targetPed = ped
-			end
-		end
-	end
+      if distance < (targetDistance or 2) then
+        targetDistance = distance
+        targetId = player
+        targetPed = ped
+      end
+    end
+  end
 
-	return targetId, targetPed
+  return targetId, targetPed
 end
 
 -- Replace ox_inventory notify with ox_lib (backwards compatibility)
 function Utils.Notify(data)
-	data.description = data.text
-	data.text = nil
-	lib.notify(data)
+  data.description = data.text
+  data.text = nil
+  lib.notify(data)
 end
 
 RegisterNetEvent('ox_inventory:notify', Utils.Notify)
 exports('notify', Utils.Notify)
 
 function Utils.ItemNotify(data)
-	if not client.itemnotify then
-		return
-	end
+  if not client.itemnotify then
+    return
+  end
 
-	SendNUIMessage({action = 'itemNotify', data = data})
+  SendNUIMessage({ action = 'itemNotify', data = data })
 end
 
 RegisterNetEvent('ox_inventory:itemNotify', Utils.ItemNotify)
 
 function Utils.DeleteObject(obj)
-	SetEntityAsMissionEntity(obj, false, true)
-	DeleteObject(obj)
+  SetEntityAsMissionEntity(obj, false, true)
+  DeleteObject(obj)
 end
 
 -- Enables the weapon wheel, but disables the use of inventory items
 -- Mostly used for weaponised vehicles, though could be called for "minigames"
 function Utils.WeaponWheel(state)
-	if state == nil then state = EnableWeaponWheel end
+  if state == nil then state = EnableWeaponWheel end
 
-	EnableWeaponWheel = state
-	SetWeaponsNoAutoswap(not state)
-	SetWeaponsNoAutoreload(not state)
+  EnableWeaponWheel = state
+  SetWeaponsNoAutoswap(not state)
+  SetWeaponsNoAutoreload(not state)
 end
+
 exports('weaponWheel', Utils.WeaponWheel)
 
 client.utils = Utils
