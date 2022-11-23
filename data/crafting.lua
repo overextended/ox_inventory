5650d197fcebc8b09f2e4d5a646b45efe50231b0
Linden <65407488+thelindat@users.noreply.github.com>
2022-11-23 14:38:13 +1100

    chore(data/crafting): second crafting location

--- data/crafting.lua
+++ data/crafting.lua
@@ -13,6 +13,7 @@ return {
 		},
 		points = {
 			vec3(-1147.083008, -2002.662109, 13.180260),
+			vec3(-345.374969, -130.687088, 39.009613)
 		},
 		zones = {
 			{
@@ -20,6 +21,12 @@ return {
 				size = vec3(3.8, 1.05, 0.15),
 				distance = 1.5,
 				rotation = 315.0,
+			},
+			{
+				coords = vec3(-346.1, -130.45, 39.0),
+				size = vec3(3.8, 1.05, 0.15),
+				distance = 1.5,
+				rotation = 70.0,
 			},
 		},
 		blip = { id = 566, colour = 31, scale = 0.8 },
