996142295191b9b4d51b6ac505707b50064f9863
Dolu <alfrider02@gmail.com>
2022-12-04 23:45:16 +0100

    fix(client/ox): only reduce value if more than 100

--- modules/bridge/ox/client.lua
+++ modules/bridge/ox/client.lua
@@ -11,7 +11,7 @@ function client.setPlayerStatus(values)
 	for name, value in pairs(values) do
 		-- Thanks to having status values setup out of 1000000 (matching esx_status's standard)
 		-- we need to awkwardly change the value
-		if value > 100 or value < 100 then value *= 0.0001 end
+		if value > 100 and value < 1000000 then value *= 0.0001 end
 
 		if value > 0 then
 			player.removeStatus(name, value)
