87a836b1fdf4690f1e0999a01b193b52431cccde
Linden <65407488+thelindat@users.noreply.github.com>
2022-12-07 20:44:57 +1100

    fix(client/ox): invert hunger and thirst

--- modules/bridge/ox/client.lua
+++ modules/bridge/ox/client.lua
@@ -12,6 +12,11 @@ function client.setPlayerStatus(values)
 		-- Thanks to having status values setup out of 1000000 (matching esx_status's standard)
 		-- we need to awkwardly change the value
 		if value > 100 or value < -100 then
+			-- Hunger and thirst start at 0 and go up to 100 as you get hungry/thirsty (inverse of ESX)
+			if (name == 'hunger' or name == 'thirst') then
+				value = -value
+			end
+
 			value = value * 0.0001
 		end
 
