d2647d83fa500527c4dc9ab7d4d8c8eb6c40672c
Galuža moree <40246702+GaluzaCZ@users.noreply.github.com>
2023-02-13 23:00:57 +0100

    Add config convar

--- modules/init.lua
+++ modules/init.lua
@@ -10,6 +10,7 @@ shared = {
 	playerweight = GetConvarInt('inventory:weight', 30000),
 	target = GetConvarInt('inventory:target', 0) == 1,
 	police = json.decode(GetConvar('inventory:police', '["police", "sheriff"]')),
+	distanceCheck = GetConvar('inventory:distancecheck', 'true') == 'true',
 }
 
 do
