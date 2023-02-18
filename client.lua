fe9cee7e5e8092084161dc4889836630d7c36296
Linden <65407488+thelindat@users.noreply.github.com>
2023-02-18 17:37:50 +1100

    fix(client): don't error on invalid updateSlots event

--- client.lua
+++ client.lua
@@ -850,11 +850,11 @@ local function updateInventory(items, weight)
 end
 
 RegisterNetEvent('ox_inventory:updateSlots', function(items, weights, count, removed)
-	if source == '' then return end
+	if source == '' or not next(items) then return end
 
-	local item = items[1].item
+	local item = items[1]?.item
 
-	if currentWeapon?.slot == item.slot and item.metadata then
+	if currentWeapon?.slot == item?.slot and item.metadata then
 		currentWeapon.metadata = item.metadata
 		TriggerEvent('ox_inventory:currentWeapon', currentWeapon)
 	end
