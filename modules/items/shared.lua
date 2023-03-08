1106be86524bee54160f8427497a6d1ec14c00fa
varkosqc <100730901+varkosqc@users.noreply.github.com>
2023-03-08 17:13:39 -0500

    Update shared.lua

--- modules/items/shared.lua
+++ modules/items/shared.lua
@@ -72,8 +72,8 @@ local function newItem(data)
 	if isServer then
 		data.client = nil
 
-		if server?.export then
-			data.cb = useExport(string.strsplit('.', server.export))
+		if serverData?.export then
+			data.cb = useExport(string.strsplit('.', serverData.export))
 		end
 
 		if not data.durability then
