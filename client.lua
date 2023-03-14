54da56d62b31295a7e96044dda38593c8ad0080e
Skryptec <48567607+Skryptific@users.noreply.github.com>
2023-03-14 22:58:37 +0100

    featcrafting/client): item metadata label

--- client.lua
+++ client.lua
@@ -1629,7 +1629,7 @@ lib.callback.register('ox_inventory:startCrafting', function(id, recipe)
 	recipe = CraftingBenches[id].items[recipe]
 
 	return lib.progressCircle({
-		label = locale('crafting_item', Items[recipe.name].label),
+		label = locale('crafting_item', recipe.metadata?.label or Items[recipe.name].label),
 		duration = recipe.duration or 3000,
 		canCancel = true,
 		disable = {
