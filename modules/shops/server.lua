dbd01bb3cffbd85ba664369e2a9b7b16b80bfd67
Manason <clanerp@gmail.com>
2023-03-25 15:54:48 -0700

    feat(server/shops): option for grade to be an array of specific grades (#1221)

--- modules/shops/server.lua
+++ modules/shops/server.lua
@@ -13,7 +13,7 @@ local locations = shared.target and 'targets' or 'locations'
 ---@field metadata? { [string]: any }
 ---@field license? string
 ---@field currency? string
----@field grade? number
+---@field grade? number | number[]
 ---@field count? number
 
 ---@class OxShopServer : OxShop
@@ -30,7 +30,7 @@ local function setupShopItems(id, shopType, shopName, groups)
 		local slot = shop.items[i]
 
 		if slot.grade and not groups then
-			print(('^1attempted to restrict slot %s (%s) to grade %s, but %s has no job restriction^0'):format(id, slot.name, slot.grade, shopName))
+			print(('^1attempted to restrict slot %s (%s) to grade %s, but %s has no job restriction^0'):format(id, slot.name, json.encode(slot.grade), shopName))
 			slot.grade = nil
 		end
 
@@ -174,6 +174,19 @@ end
 
 local TriggerEventHooks = require 'modules.hooks.server'
 
+local function isRequiredGrade(grade, rank)
+	if type(grade) == "table" then
+		for i=1, #grade do
+			if grade[i] == rank then
+				return true
+			end
+		end
+		return false
+	else
+		return rank >= grade
+	end
+end
+
 lib.callback.register('ox_inventory:buyItem', function(source, data)
 	if data.toType == 'player' then
 		if data.count == nil then data.count = 1 end
@@ -199,7 +212,7 @@ lib.callback.register('ox_inventory:buyItem', function(source, data)
 			end
 			if fromData.grade then
 				local _, rank = server.hasGroup(playerInv, shop.groups)
-				if fromData.grade > rank then
+				if not isRequiredGrade(fromData.grade, rank) then
 					return false, false, { type = 'error', description = locale('stash_lowgrade') }
 				end
 			end
