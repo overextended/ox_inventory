4423c85b7ebad4d8761a4fa22eb570bd878ce00e
Linden <65407488+thelindat@users.noreply.github.com>
2023-04-05 13:47:42 +1000

    fix(server/shops): match proper store pattern
    Properly handled stores will always contain a single space followed by
    the store id. This fixes weird and terribly formatted "single store"
    shops like "burgershotStore1".

--- modules/shops/server.lua
+++ modules/shops/server.lua
@@ -191,9 +191,10 @@ lib.callback.register('ox_inventory:buyItem', function(source, data)
 	if data.toType == 'player' then
 		if data.count == nil then data.count = 1 end
 		local playerInv = Inventory(source)
-		local shopType, shopId = playerInv.open:match("^(.-)%s-(%d-)$")
+		local shopType, shopId = playerInv.open:match('^(.-) (%d-)$')
 
 		if not shopType then shopType = playerInv.open end
+
 		if shopId then shopId = tonumber(shopId) end
 
 		local shop = shopId and Shops[shopType][shopId] or Shops[shopType]
