461a88bdc542abb123286d12bd6d693828e5b442
LukeWasTaken <39926192+LukeWasTakenn@users.noreply.github.com>
2023-04-17 10:50:15 +0200

    Revert "fix(web): rely on server updates to the slots when in game"
    This reverts commit 993ef13f17e35d0517e53ec720378f7d131b4eb5.

--- web/src/dnd/onDrop.ts
+++ web/src/dnd/onDrop.ts
@@ -4,7 +4,6 @@ import { store } from '../store';
 import { DragSource, DropTarget, InventoryType, SlotWithItem } from '../typings';
 import { moveSlots, stackSlots, swapSlots } from '../store/inventory';
 import { Items } from '../store/items';
-import { isEnvBrowser } from '../utils/misc';
 
 export const onDrop = (source: DragSource, target?: DropTarget) => {
   const { inventory: state } = store.getState();
@@ -60,9 +59,6 @@ export const onDrop = (source: DragSource, target?: DropTarget) => {
       toSlot: targetSlot.slot,
     })
   );
-
-  // Update slots if working in UI, otherwise rely on server callback
-  if (!isEnvBrowser()) return;
 
   isSlotWithItem(targetSlot, true)
     ? sourceData.stack && canStack(sourceSlot, targetSlot)
