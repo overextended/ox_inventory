overextended/ox_inventory

commit 5e468cf0e7310290a26d421d9c72f8be594cf4a7

refactor: swapItems sync tweaks

modules/inventory/server.lua
@@ -1531,19 +1531,18 @@ lib.callback.register('ox_inventory:swapItems', function(source, data)
 	local fromRef = ('%s:%s'):format(fromInventory.id, data.fromSlot)
 	local toRef = ('%s:%s'):format(toInventory.id, data.toSlot)
 
-	-- while activeSlots[fromRef] or activeSlots[toRef] do
-	-- 	Wait(0)
-	-- end
-
-	if activeSlots[fromRef] or activeSlots[toRef] then return false end
-
-	activeSlots[fromRef] = true
-	activeSlots[toRef] = true
-
-	local _ <close> = defer(function()
-		activeSlots[fromRef] = nil
-		activeSlots[toRef] = nil
-	end)
+	if activeSlots[fromRef] or activeSlots[toRef] then
+		return false, {
+			{
+				item = toInventory.items[data.toSlot] or { slot = data.toSlot },
+				inventory = toInventory.id
+			},
+			{
+				item = fromInventory.items[data.fromSlot] or { slot = data.fromSlot },
+				inventory = fromInventory.id
+			}
+		}
+	end
 
 	local sameInventory = fromInventory.id == toInventory.id
 	local fromOtherPlayer = fromInventory.player and fromInventory ~= playerInventory
@@ -1559,8 +1558,29 @@ lib.callback.register('ox_inventory:swapItems', function(source, data)
 		end
 	end
 
+	activeSlots[fromRef] = true
+	activeSlots[toRef] = true
+
+	local _ <close> = defer(function()
+		activeSlots[fromRef] = nil
+		activeSlots[toRef] = nil
+	end)
+
 	if toInventory and fromInventory and (fromInventory.id ~= toInventory.id or data.fromSlot ~= data.toSlot) then
 		local fromData = fromInventory.items[data.fromSlot]
+
+		if not fromData then
+			return false, {
+				{
+					item = { slot = data.fromSlot },
+					inventory = fromInventory.id
+				},
+				{
+					item = toData or { slot = data.toSlot },
+					inventory = toInventory.id
+				}
+			}
+		end
 
 		if fromData and (not fromData.metadata.container or fromData.metadata.container and toInventory.type ~= 'container') then
 			if data.count > fromData.count then data.count = fromData.count end
@@ -1730,11 +1750,6 @@ lib.callback.register('ox_inventory:swapItems', function(source, data)
 			local items = {}
 
 			if fromInventory.player and not fromOtherPlayer then
-				items[#items + 1] = {
-					item = fromData or { slot = data.fromSlot },
-					inventory = fromInventory.id
-				}
-
 				if toInventory.type == 'container' and containerItem then
 					items[#items + 1] = {
 						item = containerItem,
@@ -1744,11 +1759,6 @@ lib.callback.register('ox_inventory:swapItems', function(source, data)
 			end
 
 			if toInventory.player and not toOtherPlayer then
-				items[#items + 1] = {
-					item = toData or { slot = data.toSlot },
-					inventory = toInventory.id
-				}
-
 				if fromInventory.type == 'container' and containerItem then
 					items[#items + 1] = {
 						item = containerItem,
@@ -1773,21 +1783,21 @@ lib.callback.register('ox_inventory:swapItems', function(source, data)
 						item = fromInventory.items[data.fromSlot] or { slot = data.fromSlot },
 						inventory = fromInventory.id
 					}
-				}, { left = fromInventory.weight }, fromInventory ~= playerInventory)
+				}, { left = fromInventory.weight }, true)
 			else
 				toInventory:syncSlotsWithClients({
 					{
 						item = toInventory.items[data.toSlot] or { slot = data.toSlot },
 						inventory = toInventory.id
 					}
-				}, { left = toInventory.weight }, toInventory ~= playerInventory)
+				}, { left = toInventory.weight }, true)
 
 				fromInventory:syncSlotsWithClients({
 					{
 						item = fromInventory.items[data.fromSlot] or { slot = data.fromSlot },
 						inventory = fromInventory.id
 					}
-				}, { left = fromInventory.weight }, fromInventory ~= playerInventory)
+				}, { left = fromInventory.weight }, true)
 			end
 
 			local resp

client.lua
@@ -1708,6 +1708,8 @@ RegisterNUICallback('swapItems', function(data, cb)
 
 	local success, response, weaponSlot = lib.callback.await('ox_inventory:swapItems', false, data)
 
+	cb(success or false)
+
 	if success then
 		if response then
 			if weaponSlot and currentWeapon then
@@ -1717,10 +1719,12 @@ RegisterNUICallback('swapItems', function(data, cb)
 			updateInventory(response.items, response.weight)
 		end
 	elseif response then
-		lib.notify({ type = 'error', description = locale(response) })
+		if type(response) == 'table' then
+			SendNUIMessage({ action = 'refreshSlots', data = { items = response } })
+		else
+			lib.notify({ type = 'error', description = locale(response) })
+		end
 	end
-
-	cb(success or false)
 end)
 
 RegisterNUICallback('buyItem', function(data, cb)

commit 07e5e2ee570154c2d6cd324ea825c3be512ff8ba

fix(server/inventory): sync inventory with framework on successful swap
Broken by 5e468cf, as it was dependent on "next(items)", which
is now only used for container data.

modules/inventory/server.lua
@@ -1804,15 +1804,15 @@ lib.callback.register('ox_inventory:swapItems', function(source, data)
 
 			if next(items) then
 				resp = { weight = playerInventory.weight, items = items }
+			end
 
-				if server.syncInventory then
-					if fromInventory.player then
-						server.syncInventory(fromInventory)
-					end
+			if server.syncInventory then
+				if fromInventory.player then
+					server.syncInventory(fromInventory)
+				end
 
-					if toInventory.player and not sameInventory then
-						server.syncInventory(toInventory)
-					end
+				if toInventory.player and not sameInventory then
+					server.syncInventory(toInventory)
 				end
 			end
 