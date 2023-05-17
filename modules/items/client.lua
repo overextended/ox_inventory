overextended/ox_inventory

commit de1605e0b301b738ccff3c678d0cc6645e005219

refactor(client/items): __call metamethod
Server parity.

modules/items/client.lua
@@ -12,14 +12,29 @@ local function displayMetadata(metadata, value)
 end
 exports('displayMetadata', displayMetadata)
 
-local function GetItem(item)
-	if item then
-		item = string.lower(item)
-		if item:sub(0, 7) == 'weapon_' then item = string.upper(item) end
-		return Items[item]
+---@param _ table?
+---@param name string?
+---@return table?
+local function getItem(_, name)
+	if name then
+		name = name:lower()
+
+		if name:sub(0, 7) == 'weapon_' then
+			name = name:upper()
+		end
+
+		return Items[name]
 	end
+
 	return Items
 end
+
+setmetatable(Items --[[@as table]], {
+	__call = getItem
+})
+
+---@cast Items +fun(itemName: string): OxClientItem
+---@cast Items +fun(): table<string, OxClientItem>
 
 local function Item(name, cb)
 	local item = Items[name]
@@ -135,7 +150,7 @@ end)
 
 -----------------------------------------------------------------------------------------------
 
-exports('Items', GetItem)
-exports('ItemList', GetItem)
+exports('Items', getItem)
+exports('ItemList', getItem)
 
 return Items

commit 917126fbc4ddec13d019b41f7f3ba40ed7bafac5

fix(client/items): Items export wrapper function
Broken by de1605e.
Resolves #1317.

modules/items/client.lua
@@ -150,7 +150,7 @@ end)
 
 -----------------------------------------------------------------------------------------------
 
-exports('Items', getItem)
-exports('ItemList', getItem)
+exports('Items', function(item) return getItem(nil, item) end)
+exports('ItemList', function(item) return getItem(nil, item) end)
 
 return Items