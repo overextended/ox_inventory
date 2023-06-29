55fe870dc8546e060943cec9ca5604b73bd868cf
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2023-06-29 21:40:03 +0200

    Feat(Hooks): Add fromType to typeFilter

--- modules/hooks/server.lua
+++ modules/hooks/server.lua
@@ -49,7 +49,7 @@ local function TriggerEventHooks(event, payload)
 				goto skipLoop
 			end
 
-			if hook.typeFilter and not typeFilter(hook.typeFilter, payload.inventoryType or payload.shopType) then
+			if hook.typeFilter and not typeFilter(hook.typeFilter, payload.inventoryType or payload.shopType or payload.fromType) then
 				goto skipLoop
 			end
 
