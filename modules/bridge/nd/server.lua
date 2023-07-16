b772947b524e2443461d247b4c60fa7ccee5a5f0
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-07-16 12:37:31 +0200

    Update server.lua
    Don't merge, this is tested on ND 2.0.0 but update not released yet.

--- modules/bridge/nd/server.lua
+++ modules/bridge/nd/server.lua
@@ -1,15 +1,13 @@
 local playerDropped = ...
-local Inventory = require 'modules.inventory.server'
-local NDCore
+local Inventory
+local file = LoadResourceFile("ND_Core", "init.lua")
+load(file, "@ND_Core/init.lua")()
+
+CreateThread(function()
+	Inventory = server.inventory
+end)
 
 AddEventHandler("ND:characterUnloaded", playerDropped)
-
-RegisterNetEvent("ND:jobChanged", function(source, job, lastJob)
-    local inventory = Inventory(source)
-	if not inventory then return end
-	inventory.player.groups[lastJob.name] = nil
-	inventory.player.groups[job.name] = job.rank
-end)
 
 local function reorderGroups(groups)
     groups = groups or {}
@@ -19,46 +17,52 @@ local function reorderGroups(groups)
     return groups
 end
 
+local function setCharacterInventory(character)
+    character.identifier = character.id
+    character.name = ("%s %s"):format(character.firstname, character.lastname)
+    character.dateofbirth = character.dob
+    character.sex = character.gender
+    character.groups = reorderGroups(character.groups)
+    server.setPlayerInventory(character, character.inventory)
+    Inventory.SetItem(character.source, "money", character.cash)
+end
+
 SetTimeout(500, function()
-    NDCore = exports["ND_Core"]:GetCoreObject()
-    server.GetPlayerFromId = NDCore.Functions.GetPlayer
-    for _, character in pairs(NDCore.Functions.GetPlayers()) do
-        character.identifier = character.id
-        character.name = ("%s %s"):format(character.firstName, character.lastName)
-        character.dateofbirth = character.dob
-        character.sex = character.gender
-        character.groups = reorderGroups(character.data.groups)
-        server.setPlayerInventory(character, character.inventory)
-        Inventory.SetItem(character.source, "money", character.cash)
+    server.GetPlayerFromId = NDCore.getPlayer
+    for _, character in pairs(NDCore.getPlayers()) do
+        setCharacterInventory(character)
     end
 end)
 
+-- Accounts that need to be synced with physical items
+server.accounts = {
+    money = 0
+}
+
 RegisterNetEvent("ND:characterLoaded", function(character)
     if not character then return end
-    character.identifier = character.id
-    character.name = ("%s %s"):format(character.firstName, character.lastName)
-    character.dateofbirth = character.dob
-    character.sex = character.gender
-
-    local groups = reorderGroups(character.data.groups)
-    server.setPlayerInventory(character, character.inventory)
-    Inventory.SetItem(character.source, "money", character.cash)
+    setCharacterInventory(character)
 end)
 
-RegisterNetEvent("ND:moneyChange", function(player, account, amount, changeType)
+RegisterNetEvent("ND:moneyChange", function(src, account, amount, changeType, reason)
     if account ~= "cash" then return end
-    local item = Inventory.GetItem(player, "money", nil, true)
-    Inventory.SetItem(player, "money", changeType == "set" and amount or changeType == "remove" and item - amount or changeType == "add" and item + amount)
+    local item = Inventory.GetItem(src, "money", nil, true)
+    Inventory.SetItem(src, "money", changeType == "set" and amount or changeType == "remove" and item - amount or changeType == "add" and item + amount)
 end)
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function server.syncInventory(inv)
-    local accounts = Inventory.GetAccountItemCounts(inv)
+    local money = table.clone(server.accounts)
 
-    if accounts then
-        local character = NDCore.Functions.GetPlayer(inv.id)
-        NDCore.Functions.SetPlayerData(character.id, "cash", accounts.money)
+    for _, v in pairs(inv.items) do
+        if money[v.name] then
+            money[v.name] += v.count
+        end
     end
+
+    if not money then return end
+    local player = NDCore.getPlayer(inv.id)
+    player.setData("cash", money.money)
 end
 
 ---@diagnostic disable-next-line: duplicate-set-field
@@ -66,17 +70,16 @@ function server.setPlayerData(player)
     return {
         source = player.source,
         identifier = player.id,
-        name = ("%s %s"):format(player.firstName, player.lastName),
-        groups = player.data.groups,
+        name = ("%s %s"):format(player.firstname, player.lastname),
+        groups = player.groups,
         sex = player.gender,
-        dateofbirth = player.dob,
-        job = player.job
+        dateofbirth = player.dob
     }
 end
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function server.hasLicense(inv, license)
-    local character = NDCore.Functions.GetPlayer(inv.id)
+    local character = NDCore.getPlayer(inv.id)
     if not character or not character.data.licences then return end
 
     for _, characterLicense in pairs(character.data.licences) do
@@ -95,6 +98,7 @@ function server.buyLicense(inv, license)
 	end
 
 	Inventory.RemoveItem(inv, "money", license.price)
-	NDCore.Functions.CreatePlayerLicense(inv.owner, "weapon")
+    local player = NDCore.getPlayer(inv.id)
+    player.createLicense("weapon")
 	return true, "have_purchased"
 end
