195308c20397945cd2c4bb156f56b20f7fcb28cd
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-04 01:16:24 +0000

    chore: bump manifest version to v2.32.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game 'gta5'
 --[[ Resource Information ]] --
 name 'ox_inventory'
 author 'Overextended'
-version '2.32.0'
+version '2.32.1'
 repository 'https://github.com/overextended/ox_inventory'
 description 'Slot-based inventory with item metadata support'
 
