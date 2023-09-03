027e79d0b6d737181f728d6acee34e4409f17fc3
Linden <65407488+thelindat@users.noreply.github.com>
2023-09-03 13:23:06 +1000

    refactor(server/ox): tweak vehicle dbid logic

--- modules/inventory/server.lua
+++ modules/inventory/server.lua
@@ -158,18 +158,9 @@ local function loadInventoryData(data, player)
 
 			local model, class = lib.callback.await('ox_inventory:getVehicleData', source, data.netid)
 			local storage = Vehicles[data.type].models[model] or Vehicles[data.type][class]
+            local vehicleId = server.getOwnedVehicleId and server.getOwnedVehicleId(entity)
 
-			if Ox then
-				local vehicle = Ox.GetVehicle(entity)
-
-				if vehicle then
-					inventory = Inventory.Create(vehicle.id or vehicle.plate, plate, data.type, storage[1], 0, storage[2], false)
-				end
-			end
-
-			if not inventory then
-				inventory = Inventory.Create(data.id, plate, data.type, storage[1], 0, storage[2], false)
-			end
+            inventory = Inventories[vehicleId or data.id] or Inventory.Create(vehicleId or data.id, plate, data.type, storage[1], 0, storage[2], false)
 		end
 	elseif data.type == 'policeevidence' then
 		inventory = Inventory.Create(data.id, locale('police_evidence'), data.type, 100, 0, 100000, false)
@@ -577,14 +568,15 @@ function Inventory.Create(id, label, invType, slots, weight, maxWeight, owner, i
 				self.id = ('%s:%s'):format(self.id, owner)
 			end
 		else
-			if Ox then
-				self.dbId = id
-				self.id = (invType == 'glovebox' and 'glove' or invType)..label
-			else
-				self.dbId = label
-			end
+            self.dbId = id
+
+            if not invType:find(id) then
+                self.id = (invType == 'glovebox' and 'glove' or invType) .. label
+            end
 		end
 	end
+
+	Inventories[self.id] = setmetatable(self, OxInventory)
 
 	if not items then
 		self.items, self.weight = Inventory.Load(self.dbId or self.id, invType, owner, self.datastore)
@@ -592,7 +584,6 @@ function Inventory.Create(id, label, invType, slots, weight, maxWeight, owner, i
 		self.weight = Inventory.CalculateWeight(items)
 	end
 
-	Inventories[self.id] = setmetatable(self, OxInventory)
 	return Inventories[self.id]
 end
 
