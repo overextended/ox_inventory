af6bfd33cf42347e50fbbca2ac2038dddbe9f2a2
Linden <65407488+thelindat@users.noreply.github.com>
2023-09-19 06:42:02 +1000

    fix(server/inventory): prevent race condition
    Resolves #1486.

--- modules/inventory/server.lua
+++ modules/inventory/server.lua
@@ -577,14 +577,13 @@ function Inventory.Create(id, label, invType, slots, weight, maxWeight, owner, i
 		end
 	end
 
-	Inventories[self.id] = setmetatable(self, OxInventory)
-
 	if not items then
 		self.items, self.weight = Inventory.Load(self.dbId or self.id, invType, owner, self.datastore)
 	elseif weight == 0 and next(items) then
 		self.weight = Inventory.CalculateWeight(items)
 	end
 
+	Inventories[self.id] = setmetatable(self, OxInventory)
 	return Inventories[self.id]
 end
 
