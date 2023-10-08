0d7eecacb2842ff9aa6eed34134441f88d046e2d
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-10-08 13:49:33 +0200

    refactor(web): remove font awesome dependency

--- web/src/components/inventory/UsefulControls.tsx
+++ web/src/components/inventory/UsefulControls.tsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Locale } from '../../store/locale';
 import React from 'react';
 import {
@@ -41,7 +39,9 @@ const UsefulControls: React.FC<Props> = ({ infoVisible, setInfoVisible }) => {
                 <div className="useful-controls-dialog-title">
                   <p>{Locale.ui_usefulcontrols || 'Useful controls'}</p>
                   <div className="useful-controls-dialog-close" onClick={() => setInfoVisible(false)}>
-                    <FontAwesomeIcon icon={faTimes} />
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 400 528">
+                      <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                    </svg>
                   </div>
                 </div>
                 <div className="useful-controls-content-wrapper">
