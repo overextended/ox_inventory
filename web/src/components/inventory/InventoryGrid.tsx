8ea1b161fe5fc0bfd21a0453c52b128c05d070d0
ZHOURA-24 <zhourateam@gmail.com>
2023-10-15 00:15:01 +0700

    idk2

--- web/src/components/inventory/InventoryGrid.tsx
+++ web/src/components/inventory/InventoryGrid.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Inventory } from '../../typings';
 import WeightBar from '../utils/WeightBar';
 import InventorySlot from './InventorySlot';
-import InventoryContext from './InventoryContext';
 import { getTotalWeight } from '../../helpers';
-import { createPortal } from 'react-dom';
+import { useAppSelector } from '../../store';
+import { useIntersection } from '../../hooks/useIntersection';
+
+const PAGE_SIZE = 30;
 
 const InventoryGrid: React.FC<{ inventory: Inventory }> = ({ inventory }) => {
   const weight = React.useMemo(
     () => (inventory.maxWeight !== undefined ? Math.floor(getTotalWeight(inventory.items) * 1000) / 1000 : 0),
     [inventory.maxWeight, inventory.items]
   );
+  const [page, setPage] = React.useState(0);
+  const containerRef = useRef(null);
+  const { ref, entry } = useIntersection({ threshold: 0.5 });
+  const isBusy = useAppSelector((state) => state.inventory.isBusy);
+
+  React.useEffect(() => {
+    if (entry && entry.isIntersecting) {
+      setPage((prev) => ++prev);
+    }
+  }, [entry]);
   const oneToFive = inventory.items.slice(0, 5)
 
   return (
     <>
-      <div className="inventory-grid-wrapper">
+      <div className="inventory-grid-wrapper" style={{ pointerEvents: isBusy ? 'none' : 'auto' }}>
         <div className='inventory-grid-background'>
           <div className="inventory-grid-header-wrapper">
-            <p><i className={inventory.type === 'player' && 'ri-user-5-line' || inventory.type === 'shop' && 'ri-store-3-line' || inventory.type === 'crafting' && 'ri-tools-fill' || ''}></i> {inventory.label}</p>
+            <p><i className={inventory.type === 'player' && 'ri-user-5-fill' || inventory.type === 'shop' && 'ri-store-3-line' || inventory.type === 'crafting' && 'ri-tools-fill' || ''}></i> {inventory.label}</p>
             {inventory.maxWeight && (
               <p>
                 <i className="ri-handbag-line"></i>  {weight / 1000}/{inventory.maxWeight / 1000}kg
@@ -27,26 +39,38 @@ const InventoryGrid: React.FC<{ inventory: Inventory }> = ({ inventory }) => {
           </div>
           <WeightBar percent={inventory.maxWeight ? (weight / inventory.maxWeight) * 100 : 0} />
         </div>
-        <div className="inventory-grid-container">
+        <div className="inventory-grid-container" ref={containerRef}>
           <>
-            {inventory.items.map((item, index) => {
+            {inventory.items.slice(0, (page + 1) * PAGE_SIZE).map((item, index) => {
               if (index < 5 && inventory.type === 'player') {
                 return ''
               }
-              return <InventorySlot key={`${inventory.type}-${inventory.id}-${item.slot}`} item={item} inventory={inventory} />
+              return <InventorySlot
+                key={`${inventory.type}-${inventory.id}-${item.slot}`}
+                item={item}
+                ref={index === (page + 1) * PAGE_SIZE - 1 ? ref : null}
+                inventoryType={inventory.type}
+                inventoryGroups={inventory.groups}
+                inventoryId={inventory.id}
+              />
             })}
-            {inventory.type === 'player' && createPortal(<InventoryContext />, document.body)}
           </>
         </div>
-      </div >
+      </div>
       <div className={inventory.type === 'player'
         ? "one-to-five"
         : "one-to-five-hide"}
       >
-        {oneToFive.map((item) => (
-          <InventorySlot key={`${inventory.type}-${inventory.id}-${item.slot}`} item={item} inventory={inventory} />
+        {oneToFive.map((item, index) => (
+          <InventorySlot
+            key={`${inventory.type}-${inventory.id}-${item.slot}`}
+            item={item}
+            ref={index === (page + 1) * PAGE_SIZE - 1 ? ref : null}
+            inventoryType={inventory.type}
+            inventoryGroups={inventory.groups}
+            inventoryId={inventory.id}
+          />
         ))}
-        {inventory.type === 'player' && createPortal(<InventoryContext />, document.body)}
       </div>
     </>
   );
