592429858f76840d6c27dd119664143777ea30b3
Kevin Lemke <kevin1lemke@gmx.de>
2023-11-29 21:40:57 +0100

    Fixxed identification-item
    Since the item "identification" would always show "male" i fixxed it.

--- modules/items/server.lua
+++ modules/items/server.lua
@@ -306,10 +306,18 @@ function Items.Metadata(inv, item, metadata, count)
 			metadata.size = container.size
 		elseif not next(metadata) then
 			if item.name == 'identification' then
+                -- The identification item would always show "male" so I had to fix it
+                local genderText
+				if inv.player.sex == "f" then
+					genderText = "female"
+				else
+					genderText = "male"
+				end
+                
 				count = 1
 				metadata = {
 					type = inv.player.name,
-					description = locale('identification', (inv.player.sex) and locale('male') or locale('female'), inv.player.dateofbirth)
+					description = locale('identification', locale(genderText), inv.player.dateofbirth)
 				}
 			elseif item.name == 'garbage' then
 				local trashType = trash[math.random(1, #trash)]
