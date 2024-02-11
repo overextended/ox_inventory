eba2f15cb577f8b4bada7a2472b9202d96d19b55
0xDEMXN <15928886+0xDEMXN@users.noreply.github.com>
2024-02-11 15:03:49 +0100

    chore: use proper default value for useDebounce

--- web/src/hooks/useDebounce.ts
+++ web/src/hooks/useDebounce.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 
-export function useDebounce<T>(value: T, delay?: number): T {
+export function useDebounce<T>(value: T, delay: number = 500): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
     return () => {
       clearTimeout(timer);
