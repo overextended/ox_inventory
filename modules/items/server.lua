52d99285eef1dc7df31c084679db3fdf6b1c0150
Linden <65407488+thelindat@users.noreply.github.com>
2024-02-21 15:28:48 +1100

    chore(web/images): additional base images
    Credits to yaroph (see web/images/readme.md).

--- modules/items/server.lua
+++ modules/items/server.lua
@@ -10,15 +10,12 @@ Items.containers = require 'modules.items.containers'
 
 -- Possible metadata when creating garbage
 local trash = {
-	{description = 'An old rolled up newspaper.', weight = 200, image = 'trash_newspaper'},
-	{description = 'A discarded burger shot carton.', weight = 50, image = 'trash_burgershot'},
+	{description = 'A discarded burger carton.', weight = 50, image = 'trash_burger'},
 	{description = 'An empty soda can.', weight = 20, image = 'trash_can'},
 	{description = 'A mouldy piece of bread.', weight = 70, image = 'trash_bread'},
-	{description = 'An empty ciggarette carton.', weight = 10, image = 'trash_fags'},
-	{description = 'A slightly used pair of panties.', weight = 20, image = 'panties'},
-	{description = 'An empty coffee cup.', weight = 20, image = 'trash_coffee'},
-	{description = 'A crumpled up piece of paper.', weight = 5, image = 'trash_paper'},
 	{description = 'An empty chips bag.', weight = 5, image = 'trash_chips'},
+	{description = 'A slightly used pair of panties.', weight = 20, image = 'panties'},
+	{description = 'An old rolled up newspaper.', weight = 200, image = 'WEAPON_ACIDPACKAGE'},
 }
 
 ---@param _ table?
