1f803731bc3c5f15fcb04417058c09c657337262
Solareon <769465+solareon@users.noreply.github.com>
2024-08-06 10:30:02 +0200

    feat(mysql): qbx sql tables

--- modules/mysql/server.lua
+++ modules/mysql/server.lua
@@ -32,6 +32,11 @@ Citizen.CreateThreadNow(function()
         playerColumn = 'charid'
         vehicleTable = 'nd_vehicles'
         vehicleColumn = 'id'
+    elseif shared.framework == 'qbx' then
+        playerTable = 'player_inventory'
+        playerColumn = 'citizenid'
+        vehicleTable = 'player_vehicles'
+        vehicleColumn = 'id'
     end
 
     for k, v in pairs(Query) do
