f2cf021eb6324ef15f30a9cb09b69bf531743a80
Manason <clanerp@gmail.com>
2024-08-06 07:11:46 -0700

    use cached exports table

--- modules/bridge/qbx/server.lua
+++ modules/bridge/qbx/server.lua
@@ -81,7 +81,7 @@ function server.buyLicense(inv, license)
 end
 ---@diagnostic disable-next-line: duplicate-set-field
 function server.isPlayerBoss(playerId, group, grade)
-    return exports.qbx_core:IsPlayerBoss(playerId, group, grade)
+    return QBX:IsPlayerBoss(playerId, group, grade)
 end
 
 ---@param entityId number
