8c8d05708b7401a24414c2acbf73c8df5cca294a
retro <128739748+hearondale@users.noreply.github.com>
2024-09-15 03:06:18 +0100

    fix(server/crafting): update weight and sync slot when splitting stack (#1802)
    Co-authored-by: Linden <65407488+thelindat@users.noreply.github.com>

--- modules/crafting/server.lua
+++ modules/crafting/server.lua
@@ -213,6 +213,14 @@ lib.callback.register('ox_inventory:craftItem', function(source, id, index, reci
 							end
 
 							invSlot.count -= 1
+                            invSlot.weight = Inventory.SlotWeight(item, invSlot)
+
+							left:syncSlotsWithClients({
+								{
+									item = invSlot,
+									inventory = left.id
+								}
+							}, true)
 						else
                             Items.UpdateDurability(left, invSlot, item, durability < 0 and 0 or durability)
 						end
