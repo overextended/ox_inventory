ebcff1bdb210a5489b0b8610b144caac8b8f4c71
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-13 00:39:05 +0100

    fix(components/inventory): typo in InventoryGrid

--- web/src/components/inventory/InventoryGrid.tsx
+++ web/src/components/inventory/InventoryGrid.tsx
@@ -32,7 +32,7 @@ const InventoryGrid: React.FC<{ inventory: Inventory }> = ({ inventory }) => {
             <p>{inventory.label}</p>
             {inventory.maxWeight && (
               <p>
-                {weight / 1000}/{inventory.maxWeight / 1000}${Locale.kg}
+                {weight / 1000}/{inventory.maxWeight / 1000}{Locale.kg}
               </p>
             )}
           </div>
