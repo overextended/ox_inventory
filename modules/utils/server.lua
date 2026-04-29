86d560882ef55f068e56c30e6f497bc0b859371e
Linden <65407488+thelindat@users.noreply.github.com>
2026-04-30 09:27:18 +1000

    feat(utils/server): add LogExploit function

--- modules/utils/server.lua
+++ modules/utils/server.lua
@@ -5,15 +5,15 @@ local Utils = {}
 local webHook = GetConvar('inventory:webhook', '')
 
 if webHook ~= '' then
-	local validExtensions = {
-		['png'] = true,
-		['apng'] = true,
-		['webp'] = true,
-	}
+    local validExtensions = {
+        ['png'] = true,
+        ['apng'] = true,
+        ['webp'] = true,
+    }
 
-	local headers = { ['Content-Type'] = 'application/json' }
+    local headers = { ['Content-Type'] = 'application/json' }
 
-	function Utils.IsValidImageUrl(url)
+    function Utils.IsValidImageUrl(url)
         local isUri = url:match("^nui://.+")
         if isUri then
             local resource, extension = url:match('^nui://([^/]+)/.-%.(%l+)$')
@@ -36,29 +36,47 @@ if webHook ~= '' then
         end
 
         return false
-	end
+    end
 
-	---@param title string
-	---@param message string
-	---@param image string
-	function Utils.DiscordEmbed(title, message, image, color)
-		PerformHttpRequest(webHook, function() end, 'POST', json.encode({
-			username = 'ox_inventory', embeds = {
-				{
-					title = title,
-					color = color,
-					footer = {
-						text = os.date('%c'),
-					},
-					description = message,
-					thumbnail = {
-						url = image,
-						width = 100,
-					}
-				}
-			}
-		}), headers)
-	end
+    ---@param title string
+    ---@param message string
+    ---@param image string
+    function Utils.DiscordEmbed(title, message, image, color)
+        PerformHttpRequest(webHook, function() end, 'POST', json.encode({
+            username = 'ox_inventory',
+            embeds = {
+                {
+                    title = title,
+                    color = color,
+                    footer = {
+                        text = os.date('%c'),
+                    },
+                    description = message,
+                    thumbnail = {
+                        url = image,
+                        width = 100,
+                    }
+                }
+            }
+        }), headers)
+    end
+end
+
+---Prints a warning to console and logs an exploited event.
+---@param playerId number
+---@param event string
+---@param msg string
+---@param kickPlayer? boolean
+function Utils.LogExploit(playerId, event, msg, kickPlayer)
+    local warning = ('%s (%d) suspected of exploiting. %s"'):format(GetPlayerName(playerId), playerId, msg)
+    event = ('%s:%s'):format(shared.resource, event)
+
+    lib.print.warn(warning)
+    lib.logger(playerId, event, msg)
+
+    if kickPlayer then
+        DropPlayer(tostring(playerId), 'Dropped for suspicious behaviour.')
+    end
 end
 
 return Utils
