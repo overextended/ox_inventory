c326657b13e20bcf844baca07a87e84e6dc822fe
Jens Gryspeert <jens@salesnote.eu>
2026-05-11 23:45:13 +0200

    feat(mysql): qb framework db mapping
    players.citizenid + player_vehicles.plate for qb-core schema.

--- modules/mysql/server.lua
+++ modules/mysql/server.lua
@@ -32,6 +32,11 @@ Citizen.CreateThreadNow(function()
         playerColumn = 'charid'
         vehicleTable = 'nd_vehicles'
         vehicleColumn = 'id'
+    elseif shared.framework == 'qb' then
+        playerTable = 'players'
+        playerColumn = 'citizenid'
+        vehicleTable = 'player_vehicles'
+        vehicleColumn = 'plate'
     elseif shared.framework == 'qbx' then
         playerTable = 'players'
         playerColumn = 'citizenid'
