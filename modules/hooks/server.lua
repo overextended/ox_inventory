overextended/ox_inventory

commit a4ac76741b7d12d92bd87bf124f0e4a9e1bc6a69

fix(server/hooks): delay post-hook events
Unfortunately necessary due to a race condition
which requires too much refactoring to resolve.

modules/hooks/server.lua
@@ -47,6 +47,8 @@ local function TriggerEventHooks(event, payload)
 				self.success = false
 			end
 
+			Wait(50)
+
 			triggerPostEvents(self, self.success, payload)
 		end
 	})

commit 6c2b6e67efd5b22277d69f2bdd4e2be9e1197766

fix(server): prevent item duplication on disconnect mid-swap (#1950)
Resolves a bug introduced by https://github.com/overextended/ox_inventory/commit/a4ac76741b7d12d92bd87bf124f0e4a9e1bc6a69

modules/hooks/server.lua
@@ -47,9 +47,9 @@ local function TriggerEventHooks(event, payload)
 				self.success = false
 			end
 
-			Wait(50)
-
-			triggerPostEvents(self, self.success, payload)
+			SetTimeout(50, function()
+				triggerPostEvents(self, self.success, payload)
+			end)
 		end
 	})
 

modules/inventory/server.lua
@@ -654,7 +654,7 @@ function Inventory.Remove(inv)
         end
     end
 
-    if not inv.datastore and inv.changed then
+    if not inv.datastore and (inv.changed or inv.player) then
         Inventory.Save(inv)
     end
 