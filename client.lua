overextended/ox_inventory

commit d4d670bcfb6ea3f171828a4ecd53fcbefc972b5b

feat(client): setGetPlayerMethod export (#87)
* feat(client): export based setting for altering the getPlayerName method

* tweak(utils/client): removed useless luadoc inject-field rule disable

client.lua
@@ -1717,10 +1717,10 @@ RegisterNUICallback('giveItem', function(data, cb)
 			local option = nearbyPlayers[i]
 
             if isGiveTargetValid(option.ped, option.coords) then
-				local playerName = GetPlayerName(option.id)
+				local playerName = Utils.getPlayerName(option.id)
 				option.id = GetPlayerServerId(option.id)
                 ---@diagnostic disable-next-line: inject-field
-				option.label = ('[%s] %s'):format(option.id, playerName)
+				option.label = playerName
 				n += 1
 				giveList[n] = option
 			end

modules/utils/client.lua
@@ -239,5 +239,25 @@ function Utils.blurOut()
     TriggerScreenblurFadeOut(250)
 end
 
+---@param serverID number
+---@return string
+local function defaultGetPlayerName(serverID)
+    local playerName = GetPlayerName(serverID)
+    return ('[%d] %s'):format(serverID, playerName)
+end
+
+local getPlayerName = defaultGetPlayerName
+
+exports('setGetPlayerNameMethod', function (fn)
+    if type(fn) == "function" then
+        getPlayerName = fn
+    else
+        getPlayerName = defaultGetPlayerName
+    end
+end)
+
+function Utils.getPlayerName(serverId)
+    return getPlayerName(serverId)
+end
 
 return Utils

commit bd4bdbebfe48ca21c61d48084ffd7aeecf190103

fix(client): display serverId in give player list
Resolves a bug introduced by https://github.com/overextended/ox_inventory/commit/d4d670bcfb6ea3f171828a4ecd53fcbefc972b5b

client.lua
@@ -1730,8 +1730,8 @@ RegisterNUICallback('giveItem', function(data, cb)
 			local option = nearbyPlayers[i]
 
             if isGiveTargetValid(option.ped, option.coords) then
-				local playerName = Utils.getPlayerName(option.id)
 				option.id = GetPlayerServerId(option.id)
+				local playerName = Utils.getPlayerName(option.id)
                 ---@diagnostic disable-next-line: inject-field
 				option.label = playerName
 				n += 1